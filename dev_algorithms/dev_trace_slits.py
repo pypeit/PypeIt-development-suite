a3b8b3da6b5d7e260bb008899f0658dfcc691e03
profxj <xavier@ucolick.org>
2018-04-29 14:35:55 -0700

    add slit

--- dev_algorithms/dev_trace_slits.py
+++ dev_algorithms/dev_trace_slits.py
@@ -94,32 +94,51 @@ def main(pargs):
     binbpx = None
     numamplifiers = None
     saturation = None
+    add_user_slits = None
 
     settings = def_settings.copy()
     if pargs.instr == 'keck_deimos':
         spectrograph = 'keck_deimos'
         saturation = 65535.0              # The detector Saturation level
-        files = glob.glob('data/DEIMOS/DE*')
-        #files = ['../RAW_DATA/Keck_DEIMOS/830G_L/'+ifile for ifile in ['d0914_0014.fits', 'd0914_0015.fits']]
+        files = glob.glob('data/DEIMOS/DE.20100913.57*')  # Mask (57006, 57161)
+        #  The following are with sigdetect=20;  sigdetect=50 gets rid of the junk (I think)
+        #      det=1 :: 25 slits including star boxes
+        #      det=2 :: 26 slits including stars + short first one
+        #      det=3 :: 27 slits with a fake, short slit at the start
+        #      det=4 :: 27 slits with several junk slits in saturated star boxes
+        #      det=5 :: 25 slits with one junk slit on a saturated box
+        #      det=6 :: 26 slits with a short, legit first slit
+        #      det=7 :: 26 slits with stars
+        #      det=8 :: 25 slits well done slits including a short first one
+        #
+        #files = ['../RAW_DATA/Keck_DEIMOS/830G_L/'+ifile for ifile in [  # Longslit in dets 3,7
+        #    'd0914_0014.fits', 'd0914_0015.fits']]
         numamplifiers=1
 
         # Bad pixel mask (important!!)
         binbpx = ardeimos.bpm(pargs.det)
 
         #hdul = fits.open('trace_slit.fits')
-        settings['trace']['slits']['sigdetect'] = 50.0
-        settings['trace']['slits']['fracignore'] = 0.02
+        settings['trace']['slits']['sigdetect'] = 20.0
+        settings['trace']['slits']['fracignore'] = 0.0001   # 0.02 removes star boxes
         settings['trace']['slits']['pca']['params'] = [3,2,1,0]
     elif pargs.instr == 'keck_lris_red':
         spectrograph = 'keck_lris_red'
         saturation = 65535.0              # The detector Saturation level
-        #files = glob.glob('data/LRIS/Trace_flats/r15*')  # det=1 : Missed slit on amp but it wasn't the amp;  det=2 : spot on
-        #files = ['data/LRIS/Trace_flats/LR.20160110.10103.fits.gz',  # det=1: fine; det=2 : good
-        #         'data/LRIS/Trace_flats/LR.20160110.10273.fits.gz']
-        files = ['data/LRIS/Trace_flats/LR.20160110.10644.fits.gz',  # det=1: Need to add one in
-                 'data/LRIS/Trace_flats/LR.20160110.10717.fits.gz']  # det=2 better
         numamplifiers=2
 
+        files = glob.glob('data/LRIS/Trace_flats/r150420_402*')
+        #    det1 : Missing a slit between two standard stars
+        #    det2 : 12 solid slits
+        add_user_slits = [[489,563,1024]] # Goes with r150420_402*  ; and it works
+
+        #files = ['data/LRIS/Trace_flats/LR.20160110.10103.fits.gz',  # det=1: finds a ghost slit;  crazy edge case..
+        #         'data/LRIS/Trace_flats/LR.20160110.10273.fits.gz']  # det=2: solid
+
+        #files = ['data/LRIS/Trace_flats/LR.20160110.10644.fits.gz',  # det=1:  Well done! including an overlapping slit
+        #         'data/LRIS/Trace_flats/LR.20160110.10717.fits.gz']  # det=2: 21 solid slits including stars
+
+        # Read
         head0 = fits.open(files[0])[0].header
         xbin, ybin = [int(ii) for ii in head0['BINNING'].split(',')]
         binbpx = arlris.bpm(xbin, ybin, 'red', pargs.det)
@@ -131,7 +150,7 @@ def main(pargs):
         saturation = 65535.0              # The detector Saturation level
         numamplifiers=2
         #files = glob.glob('../RAW_DATA/Keck_LRIS_blue/long_600_4000_d560/b150910_2051*') # Single Twilight
-        files = glob.glob('data/LRIS/Trace_flats/LB*')  # det=1; missed/rejected the first one -- explore!! det=2 : good
+        files = glob.glob('data/LRIS/Trace_flats/LB*')  # det=1 : solid; det=2 solid [sigdetect=30]
         settings['trace']['slits']['pca']['params'] = [3,2,1,0]
         settings['trace']['slits']['sigdetect'] = 30.0
     else:
@@ -149,16 +168,13 @@ def main(pargs):
     pixlocn = artrace.gen_pixloc(mstrace, xgap, ygap, ysize)
 
     # Trace
-    #if not pargs.driver:
-    #    lcenint, rcenint, extrapord = artrace.refactor_trace_slits(pargs.det, mstrace, binbpx, pixlocn,
-    #                                                           settings=settings, pcadesc="", maskBadRows=False, min_sqm=30.)
-    #else:
-    lordloc, rordloc, extrapord = artrace.driver_trace_slits(pargs.det, mstrace, binbpx, pixlocn,
-                                                             settings=settings)
+    lordloc, rordloc, extrapord = artrace.driver_trace_slits(mstrace, pixlocn, binbpx=binbpx,
+                                                             settings=settings, add_user_slits=add_user_slits)
     # Show in Ginga?
+    nslit = lordloc.shape[1]
+    print("Found {:d} slits".format(nslit))
     if pargs.show:
         viewer, ch = ginga.show_image(mstrace)
-        nslit = lordloc.shape[1]
         ginga.show_slits(viewer, ch, lordloc, rordloc, np.arange(nslit) + 1, pstep=50)
 
     # Output to a MasterFrame?
