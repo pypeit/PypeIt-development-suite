1a61d03e6dae7c642b943b85506d990ed652e0eb
EmAstro <emanuele.paolo.farina@gmail.com>
2018-10-08 19:06:42 -0700

    trying to fix ALPHA and BETA

--- dev_algorithms/arcs2d/dev_arcs2d.py
+++ dev_algorithms/arcs2d/dev_arcs2d.py
@@ -32,19 +32,43 @@ from pypeit.core import pydl
 # The relevant vectors are:
 # - xidl['pix_nrm_xidl']
 # - xidl['t_nrm_xidl']
-# - xidl1['res_1_xidl'] -> This is the result after the first
-#                          round of fitting
-# - work2d1 -> This is the input matric for the first round
-#              of fitting.
 xidl = ascii.read('data.xidl')
 pix_nrm_xidl = np.array(xidl['pix_nrm_xidl'].data)
 t_nrm_xidl = np.array(xidl['t_nrm_xidl'].data)
 
-xidl1 = ascii.read('data1.xidl')
-res_1_xidl = np.array(xidl1['res_1_xidl'].data)
+# These are the .sav files from Feige contiaining
+# alpha, beta, p, res, work2d, and work2di for the
+# 1st and the 2nd round of fitting.
 
-work2d_1_dict = readsav('work2d1.sav', python_dict=True)
+alpha_1_dict = readsav('./sav_files/alpha1.sav', python_dict=True)
+alpha_1_xidl = alpha_1_dict['alpha']
+alpha_2_dict = readsav('./sav_files/alpha2.sav', python_dict=True)
+alpha_2_xidl = alpha_2_dict['alpha']
+
+beta_1_dict = readsav('./sav_files/beta1.sav', python_dict=True)
+beta_1_xidl = beta_1_dict['beta']
+beta_2_dict = readsav('./sav_files/beta2.sav', python_dict=True)
+beta_2_xidl = beta_2_dict['beta']
+
+p_1_dict = readsav('./sav_files/p1.sav', python_dict=True)
+p_1_xidl = p_1_dict['p']
+p_2_dict = readsav('./sav_files/p2.sav', python_dict=True)
+p_2_xidl = p_2_dict['p']
+
+res_1_dict = readsav('./sav_files/res1.sav', python_dict=True)
+res_1_xidl = res_1_dict['res']
+res_2_dict = readsav('./sav_files/res2.sav', python_dict=True)
+res_2_xidl = res_2_dict['res']
+
+work2d_1_dict = readsav('./sav_files/work2d1.sav', python_dict=True)
 work2d_1_xidl = work2d_1_dict['work2d']
+work2d_2_dict = readsav('./sav_files/work2d2.sav', python_dict=True)
+work2d_2_xidl = work2d_2_dict['work2d']
+
+work2di_1_dict = readsav('./sav_files/work2di1.sav', python_dict=True)
+work2di_1_xidl = work2di_1_dict['work2di']
+work2di_2_dict = readsav('./sav_files/work2di2.sav', python_dict=True)
+work2di_2_xidl = work2di_2_dict['work2di']
 
 # Order vector
 order = [3, 4, 5, 6, 7, 8]
@@ -167,12 +191,103 @@ print("  - min={}".format(np.min(work2d-work2d_1_xidl)))
 print("  - max={}".format(np.max(work2d-work2d_1_xidl)))
 print(" ")
 
+# Do the matrix algebra
+work2di = np.transpose(work2d * np.outer(np.ones(nocoeff*nycoeff,
+                                         dtype=float),
+                                         invvar))
+print(" ")
+print(" The shape of work2di is: {}".format(work2di.shape))
+print(" while from XIDL the shape is: {}".format(work2di_1_xidl.shape))
+print(" ")
 
-#####     # Do the matrix algebra
-#####     work2di = np.transpose(work2d * np.outer(np.ones(nocoeff*nycoeff,
-#####                                              dtype=float),invvar))
-#####     alpha = np.matmul(work2d, work2di)
-#####     beta = np.matmul(all_wv_pypeit, work2di)
+print(" ")
+print(" work2di PYPEIT vs. XIDL:")
+print("  - min={}".format(np.min(work2di-work2di_1_xidl)))
+print("  - max={}".format(np.max(work2di-work2di_1_xidl)))
+print(" ")
+
+# Plot to check that everything works fine
+plt.figure()
+plt.scatter(np.ndarray.flatten(work2d_1_xidl),
+            np.ndarray.flatten((work2d_1_xidl-work2d) * 1e8),
+            label='work2d')
+plt.scatter(np.ndarray.flatten(work2di_1_xidl),
+            np.ndarray.flatten((work2di_1_xidl-work2di) * 1e8),
+            label='work2di')
+plt.legend()
+plt.xlabel(r'RESULTS from XIDL')
+plt.ylabel(r'(XIDL - PYTHON)$\times$10$^{-8}$')
+plt.show()
+
+alpha = np.matmul(work2di.T, work2d.T)
+beta = np.matmul(work2di.T, all_wv_pypeit)
+
+# Plot to check that everything works fine
+plt.figure()
+plt.scatter(np.ndarray.flatten(alpha_1_xidl),
+            np.ndarray.flatten((alpha_1_xidl-alpha)),
+            label='alpha')
+plt.legend()
+plt.xlabel(r'RESULTS from XIDL')
+plt.ylabel(r'(XIDL - PYTHON)')
+plt.show()
+
+plt.figure()
+plt.scatter(np.ndarray.flatten(beta_1_xidl),
+            np.ndarray.flatten((beta_1_xidl-beta)),
+            label='beta')
+plt.legend()
+plt.xlabel(r'RESULTS from XIDL')
+plt.ylabel(r'(XIDL - PYTHON)')
+plt.show()
+
+plt.figure()
+plt.imshow(alpha_1_xidl-alpha)
+cbar = plt.colorbar()
+cbar.set_label('ALPHA XIDL - ALPHA PYTHON', rotation=270)
+plt.show()
+
+# Brute force test
+alpha_test = np.zeros((15,15))
+for row in range(15):
+    for column in range(15):
+        alpha_test[row,column] = np.sum(work2di[:,column]*work2d[row,:])
+filt_bf = np.abs(alpha_test-alpha) > 5e-8
+
+filt_xidl = np.abs(alpha_1_xidl-alpha) > 5e-8
+
+print(" ")
+print(" Compare np.matmul with a brute force columns times row")
+print(" multiplication. This is the opertation that is expected")
+print(" to be performed by IDL")
+print(" Are the two operations different? {}".format(np.any(filt_bf)))
+print(" Now compare the result from PYTHON with those from IDL:")
+print(" Are the two operations different? {}".format(np.any(filt_xidl)))
+print(" ")
+
+alpha_test2 = np.zeros((15,15))
+for row in range(15):
+    for column in range(15):
+        alpha_test2[column,row] = np.sum(work2di[:,row]*work2d[column,:])
+filt2 = np.abs(alpha_test2-alpha_1_xidl) > 1e-8
+
+print(" ")
+print(" The shape of alpha is: {}".format(alpha.shape))
+print(" while from XIDL the shape is: {}".format(alpha_1_xidl.shape))
+print(" The shape of beta is: {}".format(beta.shape))
+print(" while from XIDL the shape is: {}".format(beta_1_xidl.shape))
+print(" ")
+
+print(" ")
+print(" alpha PYPEIT vs. XIDL:")
+print("  - min={}".format(np.min(alpha-alpha_1_xidl)))
+print("  - max={}".format(np.max(alpha-alpha_1_xidl)))
+print(" beta PYPEIT vs. XIDL:")
+print("  - min={}".format(np.min(beta-beta_1_xidl)))
+print("  - max={}".format(np.max(beta-beta_1_xidl)))
+print(" ")
+
+
 #####     p = scipy.linalg.cholesky(alpha, lower=False)
 #####     res = scipy.linalg.cho_solve((p, False), beta)
 #####     
