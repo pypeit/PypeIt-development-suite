f32b61000d3af2467371789a9cd0d291ef79578b
joe <joe@physics.ucsb.edu>
2018-10-18 01:16:36 +0200

    Write out a line list for OH.

--- dev_algorithms/wavelengths/match_peaks.py
+++ dev_algorithms/wavelengths/match_peaks.py
@@ -59,15 +59,29 @@ def xcorr_shift_stretch(theta, y1, y2):
     #stretch = theta[1]
     y2_corr = shift_and_stretch(y2, theta[0], theta[1])
     # Zero lag correlation
-    corr_zero = -np.sum(y1*y2_corr)
+    corr_zero = np.sum(y1*y2_corr)
     corr_denom = np.sqrt(np.sum(y1*y1)*np.sum(y2*y2))
     corr_norm = corr_zero/corr_denom
     #corr = scipy.signal.correlate(y1, y2_corr, mode='same')
-    return corr_norm
+    return -corr_norm
 
-def xcross_shift(inspec1,inspec2, smooth = 5.0)
 
-    nspec = y1.shape[0]
+# This function only applies a stretch
+def xcorr_stretch(theta, y1, y2):
+
+    #shift = theta[0]
+    #stretch = theta[1]
+    y2_corr = shift_and_stretch(y2, 0.0, theta[0])
+    # Zero lag correlation
+    corr_zero = np.sum(y1*y2_corr)
+    corr_denom = np.sqrt(np.sum(y1*y1)*np.sum(y2*y2))
+    corr_norm = corr_zero/corr_denom
+    return -corr_norm
+
+
+def xcross_shift(inspec1,inspec2, smooth = 5.0):
+
+    nspec = inspec1.shape[0]
     y1 = scipy.ndimage.filters.gaussian_filter(inspec1, smooth)
     y2 = scipy.ndimage.filters.gaussian_filter(inspec2, smooth)
     lags = np.arange(-nspec + 1, nspec)
@@ -79,17 +93,92 @@ def xcross_shift(inspec1,inspec2, smooth = 5.0)
 
     return lag_max[0], corr_max_fit[0]
 
+def xcross_shift_nosm(y1,y2, debug = False):
 
-def fit_shift_stretch(inspec1, inspec2, smooth = 5.0, shift_mnmx = (-1.0,1.0), stretch_mnmx = (0.9,1.1), debug = True):
+    nspec = y1.shape[0]
+    lags = np.arange(-nspec + 1, nspec)
+    corr = scipy.signal.correlate(y1, y2, mode='full')
+    corr_denom = np.sqrt(np.sum(y1*y1)*np.sum(y2*y2))
+    corr_norm = corr/corr_denom
+    output = arc.detect_lines(corr_norm, nfitpix=7, sigdetect=5.0, FWHM=20.0, cont_samp=30, nfind = 1)
+    pix_max = output[1]
+    corr_max = np.interp(pix_max, np.arange(lags.shape[0]),corr_norm)
+    lag_max  = np.interp(pix_max, np.arange(lags.shape[0]),lags)
+    #pix_max = corr_norm.argmax()
+    #corr_max_fit, lag_max, width, cent_err = arc.fit_arcspec(lags, corr_norm, np.array([pix_max]), 7)
+    if debug:
+        # Interpolate for bad lines since the fitting code often returns nan
+        plt.figure(figsize=(14, 6))
+        plt.plot(lags, corr_norm, color='black', drawstyle = 'steps-mid', lw=3, label = 'x-corr', linewidth = 1.0)
+        plt.plot(lag_max[0], corr_max[0],'g+', markersize =6.0, label = 'peak')
+        plt.title('Best shift = {:5.3f}'.format(lag_max[0]) + ',  corr_max = {:5.3f}'.format(corr_max[0]))
+        plt.legend()
+        plt.show()
+
+    return lag_max[0], corr_max[0]
+
+
+def fit_shift_stretch(inspec1, inspec2, smooth = 3.0, shift_mnmx = (0.8,1.2), stretch_mnmx = (0.8,1.2), debug = True):
 
     nspec = inspec1.size
     y1 = scipy.ndimage.filters.gaussian_filter(inspec1, smooth)
     y2 = scipy.ndimage.filters.gaussian_filter(inspec2, smooth)
-    #guess = np.array([0.0,1.0])
-    bounds = [(nspec*shift_mnmx[0],nspec*shift_mnmx[1]), stretch_mnmx]
-    #result = scipy.optimize.minimize(xcorr_stretch, guess, args=(y1,y2), bounds=bounds)
-    result = scipy.optimize.differential_evolution(xcorr_shift_stretch, args=(y1,y2), bounds=bounds, popsize=25, recombination=0.7,
-                                                   disp=False, polish=True) # ToDO Should we polish?
+
+    # Do the cross-correlation first and determine the
+    shift_cc, cc_val = xcross_shift_nosm(y1, y2, debug = debug)
+
+    bounds = [(shift_mnmx[0]*shift_cc,shift_mnmx[1]*shift_cc), stretch_mnmx]
+    guess = np.array([shift_cc,1.0])
+    #result = scipy.optimize.minimize(xcorr_shift_stretch, guess, args=(y1,y2), bounds=bounds)
+    result = scipy.optimize.differential_evolution(xcorr_shift_stretch, args=(y1,y2), tol = 1e-4,
+                                                   bounds=bounds, disp=False, polish=True) # ToDO Should we polish?
+
+    #corr_val = xcorr_stretch((shift,stretch), y1,y2)
+    #corr = scipy.signal.correlate(y1, y2, mode='same')
+
+    if not result.success:
+        msgs.warn('Fit for shift and stretch did not converge!')
+
+
+    if debug:
+        x1 = np.arange(nspec)
+        inspec2_trans = shift_and_stretch(inspec2, result.x[0], result.x[1])
+        plt.plot(x1,inspec1, 'k-', drawstyle='steps')
+        plt.plot(x1,inspec2_trans, 'r-', drawstyle='steps')
+        plt.show()
+
+
+    return result.success, result.x[0], result.x[1], -result.fun, shift_cc, cc_val
+
+
+def fit_shift_stretch_iter(inspec1, inspec2, smooth = 5.0, shift_mnmx = (-1.0,1.0), stretch_mnmx = (0.9,1.1), debug = True):
+
+
+
+    shift_tot = 0.0
+    stretch_tot = 1.0
+    niter = 10
+
+    stretch_vec = np.linspace(stretch_mnmx[0],stretch_mnmx[1],endpoint=True, num = 100)
+    nspec = inspec1.size
+    y1 = scipy.ndimage.filters.gaussian_filter(inspec1, smooth)
+    y2 = scipy.ndimage.filters.gaussian_filter(inspec2, smooth)
+    bounds = [stretch_mnmx]
+    guess = np.array([1.0])
+
+    for iter in range(niter):
+        this_shift, corr_val = xcross_shift_nosm(y1,y2)
+        shift_tot += shift
+        y2_trans = shift_and_stretch(y2,shift,1.0)
+        corr_vec = np.zeros_like(stretch_vec)
+        for ii in np.arange(stretch_vec.shape[0]):
+            corr_vec[ii] = xcorr_stretch([stretch_vec[ii]],y1,y2_trans)
+        pix_max = corr_vec.argmax()
+        corr_max_fit, stretch_max, width, cent_err = arc.fit_arcspec(stretch_vec, -corr_vec, np.array([pix_max]), 7)
+
+        #result = scipy.optimize.minimize(xcorr_shift, guess, args=(y1,y2), bounds=bounds)
+        #result = scipy.optimize.differential_evolution(xcorr_shift_stretch, args=(y1,y2), bounds=bounds, popsize=25, recombination=0.7,
+        #                                           disp=False, polish=True) # ToDO Should we polish?
 
     #corr_val = xcorr_stretch((shift,stretch), y1,y2)
     #corr = scipy.signal.correlate(y1, y2, mode='same')
@@ -110,10 +199,13 @@ def fit_shift_stretch(inspec1, inspec2, smooth = 5.0, shift_mnmx = (-1.0,1.0), s
 
 
 
+
+
 hdu = fits.open('./spec_array.fits')
 spec = hdu[0].data.astype('float64')
 inspec1=spec[:,0]
-nspec = spec.shape[1]
+nslits = spec.shape[1]
+nspec = spec.shape[0]
 #inspec2 = spec[:,4]
 
 # TESTING
@@ -124,19 +216,17 @@ nspec = spec.shape[1]
 # Apply the shift and stretch to a copy of inspec1
 #inspec2 = shift_and_stretch(inspec1, shift, stretch)
 # evaluate the correlation coefficient at this value
-success = np.zeros(nspec,dtype=bool)
-shift = np.zeros(nspec)
-shift_cc = np.zeros(nspec)
-stretch = np.zeros(nspec)
-xcorr = np.zeros(nspec)
+success = np.zeros(nslits,dtype=bool)
+shift = np.zeros(nslits)
+shift_cc = np.zeros(nslits)
+stretch = np.zeros(nslits)
+xcorr = np.zeros(nslits)
+xcorr_cc = np.zeros(nslits)
 smooth = 5.0
 
 y1 = scipy.ndimage.filters.gaussian_filter(inspec1, smooth)
 
-for islit in range(nspec):
-    (success[islit], shift[islit], stretch[islit], xcorr[islit]) = fit_shift_stretch(inspec1,spec[:,islit], debug = True)
-    y2 = scipy.ndimage.filters.gaussian_filter(spec[:,islit], smooth)
-    corr = scipy.signal.correlate(y1, y2, mode='same')
-    lag, val = xcross_shift(inspec1,spec[:,islit])
-    shift_cc[islit] = lag
+for islit in range(nslits):
+    output = fit_shift_stretch(inspec1,spec[:,islit], debug = True)
+    success[islit], shift[islit], stretch[islit], xcorr[islit], shift_cc[islit], xcorr_cc[islit] = output
 
