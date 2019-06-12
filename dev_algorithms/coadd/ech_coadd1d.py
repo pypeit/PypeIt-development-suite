02715d2aaa58c7fe5f9f9f9bf8159a851995a12a
Feige Wang <fgwang.astro@gmail.com>
2019-06-11 18:40:27 -0700

    minor

--- dev_algorithms/coadd/ech_coadd1d.py
+++ dev_algorithms/coadd/ech_coadd1d.py
@@ -4,7 +4,8 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 
 #from pypeit.core import coadd1d
-from coadd1d_old import *
+#from coadd1d_old import *
+from pypeit.core.coadd1d import *
 from pypeit.core import load
 
 
@@ -36,19 +37,19 @@ nexp = np.size(gdobjid)
 npix = fits.getheader(fnames[0], 0)['NPIX']
 
 # giant array for storing you data
-waves = np.zeros((nexp,npix*norder))
-fluxes = np.zeros((nexp,npix*norder))
-ivars = np.zeros((nexp,npix*norder))
-masks = np.zeros((nexp,npix*norder),dtype=bool)
-scales = np.zeros((nexp,npix*norder))
-weights = np.zeros((nexp,npix*norder))
-outmasks = np.zeros((nexp,npix*norder),dtype=bool)
+waves = np.zeros((npix*norder, nexp))
+fluxes = np.zeros((npix*norder, nexp))
+ivars = np.zeros((npix*norder, nexp))
+masks = np.zeros((npix*norder, nexp),dtype=bool)
+scales = np.zeros((npix*norder, nexp))
+weights = np.zeros((npix*norder, nexp))
+outmasks = np.zeros((npix*norder, nexp),dtype=bool)
 
 # arrays to store stacked individual order spectra. I set npix_stack = npix*2 to make sure
-waves_stack_orders = np.zeros((norder,npix*2))
-fluxes_stack_orders = np.zeros((norder,npix*2))
-ivars_stack_orders = np.zeros((norder,npix*2))
-masks_stack_orders = np.zeros((norder,npix*2),dtype=bool)
+waves_stack_orders = np.zeros((npix*2, norder))
+fluxes_stack_orders = np.zeros((npix*2, norder))
+ivars_stack_orders = np.zeros((npix*2, norder))
+masks_stack_orders = np.zeros((npix*2, norder),dtype=bool)
 
 # Loop over orders to get the initial stack and scale factor or each order
 for ii, iord in enumerate(order_vec):
@@ -61,10 +62,10 @@ for ii, iord in enumerate(order_vec):
     waves_iord, fluxes_iord, ivars_iord, masks_iord = load.load_1dspec_to_array(fnames, gdobj=gdobjorder, order=iord,
                                                                            ex_value=ex_value, flux_value=flux_value)
     # store the data to the giant array
-    waves[:, ii*npix:(ii+1)*npix] = waves_iord.copy()
-    fluxes[:, ii*npix:(ii+1)*npix] = fluxes_iord.copy()
-    ivars[:, ii*npix:(ii+1)*npix] = ivars_iord.copy()
-    masks[:, ii*npix:(ii+1)*npix] = masks_iord.copy()
+    waves[ii*npix:(ii+1)*npix, :] = waves_iord.copy()
+    fluxes[ii*npix:(ii+1)*npix, :] = fluxes_iord.copy()
+    ivars[ii*npix:(ii+1)*npix, :] = ivars_iord.copy()
+    masks[ii*npix:(ii+1)*npix, :] = masks_iord.copy()
 
     # Get the stacked spectrum for each order
     # Todo: save stacked individual order spectra into one single fits
@@ -79,42 +80,56 @@ for ii, iord in enumerate(order_vec):
 
     # store individual stacked spectrum
     npix_stack = np.size(wave_stack_iord)
-    waves_stack_orders[ii,:npix_stack] = wave_stack_iord
-    fluxes_stack_orders[ii,:npix_stack] = flux_stack_iord
-    ivars_stack_orders[ii,:npix_stack] = ivar_stack_iord
-    masks_stack_orders[ii,:npix_stack] = mask_stack_iord
+    waves_stack_orders[:npix_stack, ii] = wave_stack_iord
+    fluxes_stack_orders[:npix_stack, ii] = flux_stack_iord
+    ivars_stack_orders[:npix_stack, ii] = ivar_stack_iord
+    masks_stack_orders[:npix_stack, ii] = mask_stack_iord
 
     # store new masks, scales and weights, all of these arrays are in native wave grid
-    scales[:, ii*npix:(ii+1)*npix] = scales_iord.copy()
-    weights[:, ii*npix:(ii+1)*npix] = weights_iord.copy()
-    outmasks[:, ii*npix:(ii+1)*npix] = outmask_iord.copy()
+    scales[ii*npix:(ii+1)*npix, :] = scales_iord.copy()
+    weights[ii*npix:(ii+1)*npix, :] = weights_iord.copy()
+    outmasks[ii*npix:(ii+1)*npix, :] = outmask_iord.copy()
 
+
+debug = True
+order_ratios = np.ones(norder)
 ## re-scale bluer orders to match the reddest order.
 # scaling spectrum order by order. We use the reddest order as the reference since slit loss in redder is smaller
 for ii in range(norder - 1):
     iord = norder - ii - 1
 
-    ratio = robust_median_ratio(fluxes_stack_orders[iord-1], ivars_stack_orders[iord-1], fluxes_stack_orders[iord],
-                                ivars_stack_orders[iord], ref_percentile=20.0, min_good=0.05, mask=None, mask_ref=None,
-                                maxiters=5, max_factor=10.0, sigrej=3.0)
-    print(ratio)
+    wave_blue, flux_blue, ivar_blue, mask_blue = waves_stack_orders[:, iord-1], fluxes_stack_orders[:, iord-1],\
+                                                 ivars_stack_orders[:, iord-1], masks_stack_orders[:, iord-1]
 
-    if False:
-        allok_iord = sigs[iord, :] > 0
-        allok_iordm1 = sigs[iord-1, :] > 0
-        plt.figure(figsize=(12, 6))
-        plt.plot(wave[allok], spectra.data['flux'][iord, allok], '-',lw=10,color='0.7', label='Scale region')
-        plt.plot(wave[allok_iord], spectra.data['flux'][iord,allok_iord], 'r-', label='reference spectrum')
-        plt.plot(wave[allok_iordm1], fluxes_raw[iord - 1,allok_iordm1], 'k-', label='raw spectrum')
-        plt.plot(spectra.data['wave'][iord - 1, allok_iordm1], spectra.data['flux'][iord - 1, allok_iordm1], 'b-',
-                 label='scaled spectrum')
-        mny, medy, stdy = stats.sigma_clipped_stats(fluxes[iord, allok], sigma=nsig, iters=niter)
-        plt.ylim([0.1 * medy, 4.0 * medy])
-        plt.xlim([np.min(wave[sigs[iord - 1, :] > 0]), np.max(wave[sigs[iord, :] > 0])])
+    wave_red, flux_red, ivar_red, mask_red = waves_stack_orders[:, iord], fluxes_stack_orders[:, iord]*order_ratios[iord],\
+                                             ivars_stack_orders[:, iord]*1.0/order_ratios[iord]**2, masks_stack_orders[:, iord]
+
+    # interpolate iord-1 (bluer) to iord-1 (redder)
+    flux_tmp, ivar_tmp, mask_tmp = interp_spec(wave_red, wave_blue, flux_blue, ivar_blue, mask_blue)
+
+    order_ratios[iord-1] = robust_median_ratio(flux_tmp, ivar_tmp, flux_red, ivar_red, mask=mask_tmp, mask_ref=mask_red,\
+                                               ref_percentile=20.0, min_good=0.05, maxiters=5, max_factor=10.0, sigrej=3.0)
+    msgs.info('Scaled {}th order to {}th order by {:}'.format(order_vec[iord-1],order_vec[iord],order_ratios[iord-1]))
+
+    if debug:
+        plt.figure(figsize=(12, 8))
+        plt.plot(wave_red[mask_red], flux_red[mask_red], 'k-', label='reference spectrum')
+        plt.plot(wave_blue[mask_blue], flux_blue[mask_blue],color='dodgerblue', lw=3, label='raw spectrum')
+        plt.plot(wave_blue[mask_blue], flux_blue[mask_blue]*order_ratios[iord-1], color='r',
+                 alpha=0.5, label='re-scaled spectrum')
+
+        med_width = (2.0 * np.ceil(0.03 / 2.0 * wave_blue[mask_blue].size) + 1).astype(int)
+        flux_med, ivar_med = median_filt_spec(flux_blue, ivar_blue, mask_blue, med_width)
+        ymax = 1.5 * flux_med.max()
+        ymin = -0.15 * ymax
+
+        plt.ylim([ymin, ymax])
+        plt.xlim([np.min(wave_blue[mask_blue]), np.max(wave_red[mask_red])])
         plt.legend()
         plt.xlabel('wavelength')
         plt.ylabel('Flux')
         plt.show()
+
 
 from IPython import embed
 embed()
