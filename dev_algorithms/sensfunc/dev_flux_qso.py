aeac785abc42f5369ca90cc1fe3f2f4b8b847c50
Feige Wang <fgwang.astro@gmail.com>
2019-06-28 17:29:23 -0700

    combined derving sensfunc test, apply_sensfunc test, coadd test with qso telluric test

--- dev_algorithms/sensfunc/dev_flux_qso.py
+++ dev_algorithms/sensfunc/dev_flux_qso.py
@@ -8,6 +8,7 @@ from pypeit import msgs
 
 debug = False
 show = True
+do_sens = False
 
 z_qso = 7.085
 npca = 8
@@ -17,7 +18,8 @@ qsoname = 'J1120+0641'
 datapath = os.path.join(os.getenv('HOME'), 'Dropbox/PypeIt_Redux/XSHOOTER/{:}/NIR/Science/'.format(qsoname))
 
 # TODO: change the spec1dlist to the pypeit format and change the reader accordingly
-spec1dfiles = np.genfromtxt(os.path.join(datapath,'spec1dlist'),dtype='str')
+spec1dlist = 'spec1dlist'
+spec1dfiles = np.genfromtxt(os.path.join(datapath, spec1dlist),dtype='str')
 nfiles = len(spec1dfiles)
 fnames = []
 for ifile in range(nfiles):
@@ -34,22 +36,22 @@ telgridfile = os.path.join(os.getenv('HOME'),'Dropbox/PypeIt_Redux/XSHOOTER/TelF
 
 # TODO: set sensfile=None if you want to derive sensfunc from std1dfile
 sensfile = os.path.join(os.getenv('HOME'), 'Dropbox/PypeIt_Redux/XSHOOTER/LTT3218_sens_tell.fits')
-if sensfile is None:
+if do_sens:
     if std1dfile is None:
-        msgs.error('You need either give a sensfile or a std1dfile')
+        msgs.error('You need either give a std1dfile to derive sensfunc')
     else:
         # run telluric.sensfunc_telluric to get the sensfile
-        sensfile = 'LTT3218_sens_tell.fits'
         TelSens = telluric.sensfunc_telluric(std1dfile, telgridfile, sensfile, mask_abs_lines=True, debug=debug)
 
 ## Apply the sensfunc to all spectra (only sensfunc but not tellluric)
-# TODO: change show=show
+# TODO: change show=False to show=show
 apply_sensfunc(fnames, sensfile, extinct_correct=False, tell_correct=False, debug=debug, show=False)
 
 ## Let's coadd all the fluxed spectra
 # you should get a coadded spectrum named as 'spec1d_stack_{:}.fits'.format(qsoname)
 #                a straight merge of individual order stacked spectra named as 'spec1d_merge_{:}.fits'.format(qsoname)
 #                a individual order stacked spectra (multi-extension) named as 'spec1d_order_{:}.fits'.format(qsoname)
+# TODO: change the outfile to work with datapath. It's a hard coding on these names in coadd1d
 wave_stack, flux_stack, ivar_stack, mask_stack = coadd1d.ech_combspec(fnames, objids, show=True, sensfile=sensfile,
                                                                       ex_value='OPT', outfile=qsoname, debug=debug)
 
@@ -58,5 +60,6 @@ spec1dfluxfile = 'spec1d_stack_{:}.fits'.format(qsoname)
 telloutfile = 'spec1d_stack_{:}_tellmodel.fits'.format(qsoname)
 outfile = 'spec1d_stack_{:}_tellcorr.fits'.format(qsoname)
 
+# TODO: add other modes here
 TelQSO = telluric.qso_telluric(spec1dfluxfile, telgridfile, pca_file, z_qso, telloutfile, outfile,
                                create_bal_mask=None, debug=debug, show=show)
