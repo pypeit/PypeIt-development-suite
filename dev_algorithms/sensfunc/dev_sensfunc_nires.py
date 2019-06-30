0334437abb3fe83597bf8c108feb004c249c9584
Feige Wang <fgwang.astro@gmail.com>
2019-06-29 23:10:20 -0700

    nires sensfunc

--- dev_algorithms/sensfunc/dev_sensfunc_nires.py
+++ dev_algorithms/sensfunc/dev_sensfunc_nires.py
@@ -1,59 +1,72 @@
 
 
-
-import numpy as np
-import scipy
-import matplotlib.pyplot as plt
 import os
-from sklearn import mixture
-from astropy.io import fits
-from pypeit.core import pydl
-import astropy.units as u
-from astropy.io import fits
-from astropy.table import Table
-from pypeit.core import flux
-from pypeit.core import load
-from pypeit.core import save
-from pypeit.core import coadd2d
-from pypeit.core import coadd1d
-from pypeit.spectrographs import util
-from pypeit import utils
-from pypeit import msgs
-import pickle
-PYPEIT_FLUX_SCALE = 1e-17
-from astropy.io import fits
-import copy
+import numpy as np
+
 import telluric
-import IPython
+from flux1d import apply_sensfunc
+from pypeit.core import coadd1d
+from pypeit import msgs
 
-
-dev_path = os.getenv('PYPEIT_DEV')
+debug = False
+show = True
 do_sens = False
-do_qso = True
 
+z_qso = 7.50
+npca = 8
+ex_value = 'OPT'
+qsoname = 'BlueHawaii'
 
-##################################################
-# Example of sensitivity function determination  #
-##################################################
+datapath = os.path.join(os.getenv('HOME'), 'Dropbox/PypeIt_Redux/NIRES/NIRES_May19/Science/')
+
+# TODO: change the spec1dlist to the pypeit format and change the reader accordingly
+spec1dlist = 'spec1dlist'
+spec1dfiles = np.genfromtxt(os.path.join(datapath, spec1dlist),dtype='str')
+nfiles = len(spec1dfiles)
+fnames = []
+for ifile in range(nfiles):
+    fnames.append(os.path.join(datapath,spec1dfiles[ifile]))
+
+#TODO: the objids shoul be read in from the pypeit format file as noted above.
+objids = ['OBJ0001']*nfiles
+
+std1dfile = os.path.join(os.getenv('HOME'),
+                          'Dropbox/PypeIt_Redux/NIRES/NIRES_May19/Science/spec1d_s190519_0059-GD153_NIRES_2019May19T083811.995.fits')
+
+# get the pca pickle file and atmosphere model grid
+pca_file = os.path.join(os.getenv('HOME'),'Dropbox/PypeIt_Redux/qso_pca_1200_3100.pckl')
+telgridfile = os.path.join(os.getenv('HOME'),'Dropbox/PypeIt_Redux/TelFit_MaunaKea_3100_26100_R20000.fits')
+
+# TODO: set sensfile=None if you want to derive sensfunc from std1dfile
+sensfile = os.path.join(os.getenv('HOME'), 'Dropbox/PypeIt_Redux/NIRES/GD153_sens_tell_nires.fits')
 if do_sens:
-    spec1dfile = '/d2/Feige/Dropbox/OBS_DATA/NIRES/ut190519/Science/spec1d_s190519_0059-GD153_NIRES_2019May19T083811.995.fits'
-    telgridfile = '/d2/Feige/Dropbox/TellGrid/TelFit_MaunaKea_3100_26100_R20000.fits'
-    outfile = 'GD153_sens_tell_nires.fits'
-    TelSens = telluric.sensfunc_telluric(spec1dfile, telgridfile, outfile, mask_abs_lines=True,debug=False)
+    if std1dfile is None:
+        msgs.error('You need either give a std1dfile to derive sensfunc')
+    else:
+        # run telluric.sensfunc_telluric to get the sensfile
+        TelSens = telluric.sensfunc_telluric(std1dfile, telgridfile, sensfile, mask_abs_lines=True, debug=debug)
 
+## Apply the sensfunc to all spectra (only sensfunc but not tellluric)
+# TODO: change show=False to show=show
+apply_sensfunc(fnames, sensfile, extinct_correct=False, tell_correct=False, debug=debug, show=False)
 
-###############################
-# Example of quasar model fit #
-###############################
-if do_qso:
-    spec1dfluxfile = os.path.join(os.getenv('HOME'),'Dropbox/PypeIt_Redux/XSHOOTER/NIR_Stack/spec1d_stack_J0224-4711.fits')
-    pca_file = os.path.join(os.getenv('HOME'),'Dropbox/PypeIt_Redux//qso_pca_1200_3100.pckl')
-    telgridfile = os.path.join(os.getenv('HOME'),'Dropbox/PypeIt_Redux/XSHOOTER/TelFit_Paranal_NIR_9800_25000_R25000.fits')
-    telloutfile = 'J0224-4711_tell.fits'
-    outfile = 'spec1d_stack_tell_J0224-4711.fits'
-    z_qso = 6.51
-    TelQSO = telluric.qso_telluric(spec1dfluxfile, telgridfile, pca_file, z_qso, telloutfile, outfile,
-                                   create_bal_mask=None, debug=True, show=True)
+fnames_flux = [f.replace('.fits', '_flux.fits') for f in fnames]
 
+## Let's coadd all the fluxed spectra
+# you should get a coadded spectrum named as 'spec1d_stack_{:}.fits'.format(qsoname)
+#                a straight merge of individual order stacked spectra named as 'spec1d_merge_{:}.fits'.format(qsoname)
+#                a individual order stacked spectra (multi-extension) named as 'spec1d_order_{:}.fits'.format(qsoname)
+# TODO: change the outfile to work with datapath. It's a hard coding on these names in coadd1d
+wave_stack, flux_stack, ivar_stack, mask_stack = coadd1d.ech_combspec(fnames_flux, objids, show=show, sensfile=sensfile,
+                                                                      ex_value='OPT', outfile=qsoname, debug=debug)
+
+# run telluric.qso_telluric to get the final results
+spec1dfluxfile = 'spec1d_stack_{:}.fits'.format(qsoname)
+telloutfile = 'spec1d_stack_{:}_tellmodel.fits'.format(qsoname)
+outfile = 'spec1d_stack_{:}_tellcorr.fits'.format(qsoname)
+
+# TODO: add other modes here
+TelQSO = telluric.qso_telluric(spec1dfluxfile, telgridfile, pca_file, z_qso, telloutfile, outfile,
+                               create_bal_mask=None, debug=True, show=show)
 
 
