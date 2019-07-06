a77e0092b2223d884599454af95307035535f325
Feige Wang <fgwang.astro@gmail.com>
2019-07-06 00:02:16 -0700

    dev flux vis

--- dev_algorithms/sensfunc/dev_flux_vis.py
+++ dev_algorithms/sensfunc/dev_flux_vis.py
@@ -15,7 +15,7 @@ npca = 8
 ex_value = 'OPT'
 qsoname = 'J0224-4711'
 
-datapath = os.path.join(os.getenv('HOME'), 'Dropbox/PypeIt_Redux/XSHOOTER/J0224-4711/pypeit/Science/')
+datapath = os.path.join(os.getenv('HOME'), 'Dropbox/PypeIt_Redux/XSHOOTER/J0224-4711/VIS/Science/')
 
 # TODO: change the spec1dlist to the pypeit format and change the reader accordingly
 spec1dlist = 'spec1dlist'
@@ -28,7 +28,7 @@ for ifile in range(nfiles):
 #TODO: the objids shoul be read in from the pypeit format file as noted above.
 objids = ['OBJ0001']*nfiles
 
-std1dfile = os.path.join(os.getenv('HOME'),'Dropbox/PypeIt_Redux/XSHOOTER/J0224-4711/pypeit/Science/spec1d_XSHOO.2018-01-18T08:43:18.841-LTT3218_XShooter_VIS_2018Jan18T084318.841.fits')
+std1dfile = os.path.join(os.getenv('HOME'),'Dropbox/PypeIt_Redux/XSHOOTER/J0224-4711/VIS/Science/spec1d_XSHOO.2017-11-23T08:31:41.418-LTT3218_XShooter_VIS_2017Nov23T083141.419.fits')
 
 # get the pca pickle file and atmosphere model grid
 pca_file = os.path.join(os.getenv('HOME'),'Dropbox/PypeIt_Redux/qso_pca_1200_3100.pckl')
@@ -45,7 +45,7 @@ if do_sens:
 
 ## Apply the sensfunc to all spectra (only sensfunc but not tellluric)
 # TODO: change show=False to show=show
-#apply_sensfunc(fnames, sensfile, extinct_correct=False, tell_correct=False, debug=debug, show=False)
+apply_sensfunc(fnames, sensfile, extinct_correct=False, tell_correct=False, debug=debug, show=False)
 
 fnames_flux = [f.replace('.fits', '_flux.fits') for f in fnames]
 
