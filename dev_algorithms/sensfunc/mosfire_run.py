90bfd807af7f06098a955f70694021fe2bdcc9aa
Feige Wang <fgwang.astro@gmail.com>
2019-11-19 13:02:56 +0800

    mosfire flux

--- dev_algorithms/sensfunc/mosfire_run.py
+++ dev_algorithms/sensfunc/mosfire_run.py
@@ -9,11 +9,18 @@ basedir = '/d2/Feige/'
 
 std_path = os.path.join(basedir,'Dropbox/PypeIt_Redux/MOSFIRE/Nov19/nov_redux/Science')
 stdfile = 'spec1d_m191118_0064-GD71_MOSFIRE_2019Nov18T104704.507.fits'
-tell_method = 'qso'
 
-## J0313-1806
-sci_path = os.path.join(basedir,'Dropbox/PypeIt_Redux/MOSFIRE/Nov19/nov_redux/Science')
+tell_method = 'qso'
+#tell_method = 'poly'
+
+
 z_qso = 7.6
-fileroot = 'J0313-1806_FIRE'
-flux_tell(sci_path, stdfile, std_path=std_path,instrument=instrument, fileroot=fileroot, z_qso=z_qso, tell_method=tell_method,
+sci_path = os.path.join(basedir,'Dropbox/PypeIt_Redux/MOSFIRE/Nov19/nov_redux/Science')
+spec1dfiles = ['spec1d_d0527_0080-P261_OFF_DEIMOS_2017May27T102635.318.fits',
+               'spec1d_d0527_0080-P261_OFF_DEIMOS_2017May27T102635.318.fits']
+objids = ['SPAT0764-SLIT0000-DET03','SPAT0764-SLIT0000-DET07']
+outroot = 'J1724+1901_MOSFIRE_2019nov18'
+flux_tell(sci_path, stdfile, spec1dfiles=spec1dfiles, std_path=std_path, instrument=instrument,
+          outroot=outroot, objids=objids,  z_qso=z_qso, tell_method=tell_method,
+          fit_region_min=[9200.0], fit_region_max=[9900.0],
           do_sens=True, do_flux=False, do_stack=False, do_tell=False, disp=False, debug=False)
