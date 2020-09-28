c3b39c8e65be6647e784c3f39f814aa8355341ad
profxj <xavier@ucolick.org>
2020-09-28 07:30:35 -0700

    one slit

--- dev_algorithms/ql/test_deimos_ql.py
+++ dev_algorithms/ql/test_deimos_ql.py
@@ -10,6 +10,12 @@ def afternoon():
     # Run
     ql_keck_deimos.main(pargs)
 
+def one_slit():
+    cmds = [os.path.join(os.environ("PYPEIT_DEV"), 'DEIMOS_QL_TST', 'raw'),
+            '--science DE.20130409.20629.fits',  '--slit_spat 3:175']
+    pargs = ql_keck_deimos.parse_args(cmds)
+    # Run
+    ql_keck_deimos.main(pargs)
 
 # Command line execution
 if __name__ == '__main__':
