dfeefb5d4bdfa67c14bf1865998b945d9724292c
joe <joe@physics.ucsb.edu>
2020-12-07 12:22:27 +0100

    Updated build_cooked script to accomodate new output format.

--- dev_algorithms/mosfire/mosfire_photometry.py
+++ dev_algorithms/mosfire/mosfire_photometry.py
@@ -1,13 +1,13 @@
-from astropy.io import fits
-from pypeit.display import display
+import os
 import numpy as np
-from photutils import RectangularAperture, aperture_photometry
-from astropy.stats import sigma_clipped_stats
 from scipy.ndimage import gaussian_filter
-from photutils import DAOStarFinder
+from astropy.io import fits
+from astropy.stats import sigma_clipped_stats
 from pypeit.images import buildimage
+from pypeit.display import display
 from pypeit.spectrographs.util import load_spectrograph
-
+from photutils import RectangularAperture, aperture_photometry
+from photutils import DAOStarFinder
 
 # The following code is from MOSFIRE.CSU
 def mm_to_pix(millimeters):
@@ -65,10 +65,11 @@ ZP = {
 #star_obj_file = '/Users/joe/Downloads/MF.20200528.40413.fits'
 #star_sky_file = '/Users/joe/Downloads/MF.20200528.40360.fits'
 
-qso_obj_file = '/home/riccardo/Downloads/m201023_0093.fits'
-qso_sky_file = '/home/riccardo/Downloads/m201023_0092.fits'
-star_obj_file = '/home/riccardo/Downloads/m201023_0091.fits'
-star_sky_file = '/home/riccardo/Downloads/m201023_0090.fits'
+path = '/Users/joe/Dropbox/MOSFIRE_acq/'
+qso_obj_file = os.path.join(path, 'm201023_0093.fits')
+qso_sky_file = os.path.join(path, 'm201023_0092.fits')
+star_obj_file = os.path.join(path, 'm201023_0091.fits')
+star_sky_file = os.path.join(path, 'm201023_0090.fits')
 
 
 # Build Science image
@@ -77,9 +78,22 @@ det = 1
 parset = spectrograph.default_pypeit_par()
 parset['scienceframe']['process']['use_illumflat'] = False
 parset['scienceframe']['process']['use_pixelflat'] = False
+# Do not reject cosmics for now, it is slow
+parset['scienceframe']['process']['mask_cr'] = False
 
-skyImg = buildimage.buildimage_fromlist(
+qsoSkyImg = buildimage.buildimage_fromlist(
     spectrograph, det, parset['scienceframe'], [qso_sky_file], ignore_saturation=False)
+qsoImg = buildimage.buildimage_fromlist(
+    spectrograph, det, parset['scienceframe'], [qso_obj_file], ignore_saturation=False)
+starSkyImg = buildimage.buildimage_fromlist(
+    spectrograph, det, parset['scienceframe'], [star_sky_file], ignore_saturation=False)
+starImg = buildimage.buildimage_fromlist(
+    spectrograph, det, parset['scienceframe'], [star_obj_file], ignore_saturation=False)
+qso_sky_hdr = qsoSkyImg.rawheadlist[0]
+star_sky_hdr = starSkyImg.rawheadlist[0]
+plate_scale = qsoSkyImg.detector.platescale
+nspec, nspat = qsoSkyImg.shape
+
 sys.exit(-1)
 # Read in data
 #star_obj = fits.getdata(star_obj_file)
@@ -90,24 +104,37 @@ sys.exit(-1)
 #qso_obj = fits.getdata(qso_obj_file)
 #qso_hdr =fits.getheader(qso_obj_file)
 #qso_sky = fits.getdata(qso_sky_file)
-qso_sky_hdr =fits.getheader(qso_sky_file)
-qso_diff = qso_obj - qso_sky
+#qso_sky_hdr =fits.getheader(qso_sky_file)
+qso_diff = qsoImg.image - qsoSkyImg.image
+star_diff = starImg.image - starSkyImg.image
+#qso_sky = fits.getdata(qso_sky_file)
+
+# define crude spec, spat boundaries
+spat_coord, spec_coord = np.meshgrid(np.arange(nspat), np.arange(nspec))
+upper_left_spec, upper_left_spat = (1058, 1032)
+upper_right_spec, upper_right_spat = (1054, 1071.3)
+lower_left_spec, lower_left_spat = (1032, 1035)
+lower_right_spec, lower_right_spat = (1032,1072)
+median_box = (spat_coord > lower_left_spat) & (spat_coord < upper_right_spat) & (spec_coord > lower_left_spec) & (spec_coord < upper_right_spec)
+mean_sky, med_sky, sigma_sky = sigma_clipped_stats(starSkyImg.image[median_box], sigma_lower=5.0, sigma_upper=5.0)
+acq_box = (spat_coord > lower_left_spat) & (spat_coord < upper_right_spat) & \
+          (starSkyImg.image > (med_sky - 3.0*sigma_sky)) & (starSkyImg.image < (med_sky + 3.0*sigma_sky))
 
 # Get bar information from header
-xpix = np.zeros(92)
-ypix = np.zeros(92)
-for islit in range(1, 93):
-    slit = int(islit + 1) / 2
-    pos = qso_sky_hdr["B%0.2iPOS" % islit]
-    xpix[islit-1], ypix[islit-1] = csu_mm_to_pix(pos, slit)
+#xpix = np.zeros(92)
+#ypix = np.zeros(92)
+#for islit in range(1, 93):
+#    slit = int(islit + 1) / 2
+#    pos = qso_sky_hdr["B%0.2iPOS" % islit]
+#    xpix[islit-1], ypix[islit-1] = csu_mm_to_pix(pos, slit)
 
 display.connect_to_ginga(raise_err=True, allow_new=True)
-viewer, ch_sky = display.show_image(qso_diff,'QSO_SKY') #, cuts = (-5.0*qso_sigma, 5.0*qso_sigma))
+viewer, ch_sky = display.show_image(qsoSkyImg.image,'QSO_SKY') #, cuts = (-5.0*qso_sigma, 5.0*qso_sigma))
 display.show_points(viewer, ch_sky, ypix, xpix)
 
 sys.exit(-1)
 
-plate_scale = 0.18 # Put in correct value
+
 centroid=[star_hdr['CRPIX1']+15,star_hdr['CRPIX2']+20]
 lengthx=round(2/plate_scale)
 lengthy=round(3.5/plate_scale)
@@ -154,16 +181,44 @@ counts_back_qso=(phot_back_qso['aperture_sum']-phot_qso['aperture_sum'])/(back_a
 f_qso=(counts_qso-counts_back_qso)/qso_hdr['ELAPTIME']
 m_qso=-2.5 * np.log10(f_qso) + ZP[qso_hdr['FILTER']]
 print(f_qso,m_qso)
-'''
+
 # Display images
-display.connect_to_ginga(raise_err=True, allow_new=True)
-viewer, ch_star = display.show_image(star_diff,'Star', clear=True, cuts = (-5.0*star_sigma, 5.0*star_sigma))
-viewer, ch_qso = display.show_image(qso_diff,'QSO', cuts = (-5.0*qso_sigma, 5.0*qso_sigma))
-out_star = ch_star.cut_levels((star_mean -5.0*star_sigma, star_mean + 10.0*star_sigma))
-out_qso = ch_qso.cut_levels((qso_mean -5.0*qso_sigma, qso_mean + 7.0*qso_sigma))
+#display.connect_to_ginga(raise_err=True, allow_new=True)
+#viewer, ch_star = display.show_image(star_diff,'Star', clear=True, cuts = (-5.0*star_sigma, 5.0*star_sigma))
+#viewer, ch_qso = display.show_image(qso_diff,'QSO', cuts = (-5.0*qso_sigma, 5.0*qso_sigma))
+#out_star = ch_star.cut_levels((star_mean -5.0*star_sigma, star_mean + 10.0*star_sigma))
+#out_qso = ch_qso.cut_levels((qso_mean -5.0*qso_sigma, qso_mean + 7.0*qso_sigma))
 
 # After displaying all the images sync up the images with WCS_MATCH
-shell = viewer.shell()
-shell.start_global_plugin('WCSMatch')
-shell.call_global_plugin_method('WCSMatch', 'set_reference_channel', [ch_star], {})
-'''
+#shell = viewer.shell()
+#shell.start_global_plugin('WCSMatch')
+#shell.call_global_plugin_method('WCSMatch', 'set_reference_channel', [ch_star], {})
+#'''
+
+
+#trap_left_spec = lower_left_spec + (spat_coord - lower_left_spat)*(upper_left_spec - lower_left_spec)/(upper_left_spat - lower_left_spat)
+#trap_right_spec = lower_right_spec + (spat_coord - lower_right_spat)*(upper_right_spec - lower_right_spec)/(upper_right_spat - lower_right_spat)
+#trap_top_spec = upper_left_spec + (spat_coord - upper_left_spat)*(upper_right_spec - upper_left_spec)/(upper_right_spat - upper_left_spat)
+#trap_bottom_spec = lower_left_spec + (spat_coord - lower_left_spat)*(lower_right_spec - lower_left_spec)/(lower_right_spat - lower_left_spat)
+
+#trap_left_spat = lower_left_spat + (spec_coord - lower_left_spec)*(upper_left_spat - lower_left_spat)/(upper_left_spec - lower_left_spec)
+#trap_right_spat = lower_right_spat + (spat_coord - lower_right_spec)*(upper_right_spat - lower_right_spat)/(upper_right_spec - lower_right_spec)
+#trap_top_spat = upper_left_spat + (spec_coord - upper_left_spec)*(upper_right_spat - upper_left_spat)/(upper_right_spec - upper_left_spec)
+#trap_bottom_spat = lower_left_spat + (spec_coord - lower_left_spec)*(lower_right_spat - lower_left_spat)/(lower_right_spec - lower_left_spec)
+
+#acq_img = (spat_coord > trap_left_spat) & (spec_coord < trap_right_spat) & (spec_coord > trap_bottom_spec) & (spec_coord < trap_top_spec)
+
+#in_left = (spec_coord > lower_left_spec) & (spec_coord < upper_left_spec)
+#in_right = (spec_coord > lower_right_spec) & (spec_coord < upper_right_spec)
+#in_top = (spat_coord > upper_left_spat) & (spat_coord < upper_right_spat)
+#in_bottom = (spat_coord > lower_left_spat) & (spat_coord < lower_right_spat)
+#trap_left[np.logical_not(in_left)] = np.inf
+#trap_right[np.logical_not(in_left)] = np.inf
+#trap_top[np.logical_not(in_top)] = np.inf
+#trap_bottom[np.logical_not(in_bottm)] = np.inf
+#acq_img =
+
+
+#acq_box = (spat_coord > lower_left_spat) & (spat_coord < )
+
+
