c944cc33bf36d248eab1a27cbdc084c973888a94
joe <joe@physics.ucsb.edu>
2021-04-22 17:09:52 -0700

    Added mosfire sensfunc for Y_long

---
+++ dev_algorithms/hires/hires2specobj.py
@@ -0,0 +1,9 @@
+
+from astropy.io import fits
+# Task for you is to read in the standard star reduction from Extract directory and make some plots of the
+# of the counts vs wavelength for each order. The relevant tags are wave, fx, var (sqrt var is sigma)
+
+# Extra credit (you probably won't get this)
+
+# Try to create a SpecObjs in echelle format that contains the data from that standard file. Look at echelle in Dev Suite forPypeit for examples. 
+
