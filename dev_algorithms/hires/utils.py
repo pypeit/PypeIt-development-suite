94a049c2dcd334946d67a454a40feab3e8afc3b9
joe <joe@physics.ucsb.edu>
2021-05-13 16:43:30 -0700

    Added mosfire sensfunc for Y_long

--- dev_algorithms/hires/utils.py
+++ dev_algorithms/hires/utils.py
@@ -1,15 +1,6 @@
 from matplotlib import pyplot as plt
 from astropy.io import fits
 import numpy as np
-from pypeit import specobj
-# Task for you is to read in the standard star reduction from Extract directory and make some plots of the
-# of the counts vs wavelength for each order. The relevant tags are wave, fx, var (sqrt var is sigma)
-
-# Extra credit (you probably won't get this)
-
-# Try to create a SpecObjs in echelle format that contains the data from that standard file. Look at echelle in Dev Suite forPypeit for examples. 
-
-
 
 
 def plot_cts(fitsfile):
@@ -21,7 +12,7 @@ def plot_cts(fitsfile):
     var = data['var']
     order = data['order']
 
-    plt.figure(figsize=(10,7))
+    plt.figure(figsize=(10 ,7))
     for i in range(len(order)):
         iwant = np.argwhere(wave[i] != 0)
         plt.plot(wave[i][iwant], fx[i][iwant], label=order[i])
