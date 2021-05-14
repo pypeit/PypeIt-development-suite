d907af3dbba37b34578709a30b46820bcf823390
joe <joe@physics.ucsb.edu>
2021-05-13 17:24:09 -0700

    First bits

--- dev_algorithms/hires/hires2specobj.py
+++ dev_algorithms/hires/hires2specobj.py
@@ -2,6 +2,9 @@ from matplotlib import pyplot as plt
 from astropy.io import fits
 import numpy as np
 from pypeit import specobj
+from pypeit import specobjs
+from pypeit import utils
+from pypeit.spectrographs.util import load_spectrograph
 # Task for you is to read in the standard star reduction from Extract directory and make some plots of the
 # of the counts vs wavelength for each order. The relevant tags are wave, fx, var (sqrt var is sigma)
 
@@ -9,29 +12,32 @@ from pypeit import specobj
 
 # Try to create a SpecObjs in echelle format that contains the data from that standard file. Look at echelle in Dev Suite forPypeit for examples. 
 
+# Read in a Cowie xidl output for a single exposure
+xidl_file = '/Users/joe/HIRES_redux/J0100+2802/Cowie/Extract/Obj_0147R.fits.gz'
+head2d = fits.getheader(xidl_file, ext=1)
+
+hdu = fits.open(xidl_file)
+data = hdu[1].data
+
+# TODO Implement all detectors
+norders =len(data)
+sobjs = specobjs.SpecObjs()
+for iord in range(norders-1,-1,-1):
+    thisobj = specobj.SpecObj('Echelle', 1, OBJTYPE='science', ECH_ORDERINDX=iord, ECH_ORDER=data[iord]['ORDER'])
+    # Set boxcar extracted flux
+    thisobj.BOX_WAVE = data[iord]['BOX_WV']
+    thisobj.BOX_COUNTS = data[iord]['BOX_FX']
+    thisobj.BOX_COUNTS_IVAR = utils.inverse(data[iord]['BOX_VAR'])
+    thisobj.BOX_COUNTS_SIG = np.sqrt(data[iord]['BOX_VAR'])
+    thisobj.BOX_MASK = data[iord]['BOX_VAR'] > 0.0
+    # Do the same things for the opt flags
+    thisobj.set_name()
+    sobjs.add_sobj(thisobj)
 
 
-
-def plot_cts(fitsfile):
-    # fitsfile can be 'fits.gz'
-
-    data = fits.open(fitsfile)[1].data
-    wave = data['wave']
-    fx = data['fx']
-    var = data['var']
-    order = data['order']
-
-    plt.figure(figsize=(10,7))
-    for i in range(len(order)):
-        iwant = np.argwhere(wave[i] != 0)
-        plt.plot(wave[i][iwant], fx[i][iwant], label=order[i])
-
-    plt.xlabel('Wavelength (A)')
-    plt.ylabel('Counts')
-    plt.legend()
-    plt.ylim([-100, np.max(fx)])
-    plt.tight_layout()
-    plt.show()
-
-
+spectrograph = load_spectrograph('keck_hires')
+# Somehow hack the fitstbl or whatever
+subheader = spectrograph.subheader_for_spec(row_fitstbl, head2d)
+outfile = 'test.fits'
+sobjs.write_to_fits(subheader, outfile)
 
