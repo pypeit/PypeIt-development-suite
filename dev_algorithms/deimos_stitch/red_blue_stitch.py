e0d8e50fe94891d7ae0cc67544ce6136c3cd727f
ShaneBechtel <shane.bechtel@yahoo.com>
2021-09-08 19:59:38 -0700

    More progress on stitching code.

--- dev_algorithms/deimos_stitch/red_blue_stitch.py
+++ dev_algorithms/deimos_stitch/red_blue_stitch.py
@@ -39,16 +39,19 @@ def ivarsmooth(flux, ivar, window):
     return (smoothflux, outivar)
 
 
-#spec_file = "/home/sbechtel/Documents/DEIMOS_Light_Echo/Targets/J1438A/det_all/setup_Both/Science_coadd/spec1d_DE.20190605.30172-DE.20190605.35227-J1438A.fits"
+# spec_file = "/home/sbechtel/Documents/DEIMOS_Light_Echo/Targets/J1438A/det_all/setup_Both/Science_coadd/spec1d_DE.20190605.30172-DE.20190605.35227-J1438A.fits"
 # Extens = 9, 30
 
 
-spec_file = "/home/sbechtel/Documents/DEIMOS_Light_Echo/Targets/J1438A/det_all/setup_Star/Science_coadd/spec1d_DE.20190605.30172-DE.20190605.35227-J1438A.fits"
+# spec_file = "/home/sbechtel/Documents/DEIMOS_Light_Echo/Targets/J1438A/det_all/setup_Star/Science_coadd/spec1d_DE.20190605.30172-DE.20190605.35227-J1438A.fits"
 # Extens = 10, 25
 
+spec_file = "/home/sbechtel/Documents/DEIMOS_Light_Echo/Targets/J1630A/det_all/setup_Both/Science_coadd/spec1d_DE.20190705.25097-DE.20190705.36380-J1630A.fits"
+# Extens = 6, 51
+
 spec_hdu = fits.open(spec_file)
-blue_data = spec_hdu[10].data
-red_data = spec_hdu[25].data
+blue_data = spec_hdu[6].data
+red_data = spec_hdu[51].data
 
 blue_waves = blue_data['OPT_WAVE']
 blue_flux = blue_data['OPT_COUNTS']
@@ -111,12 +114,11 @@ wgmax = np.max(red_waves)
 wgmin = np.min(blue_waves[blue_waves > 10])
 new_waves, new_flux, new_ivars, new_masks = multi_combspec(waves, fluxes, ivars, masks, wave_grid_max=wgmax,
                                                            wave_grid_min=wgmin)
-'''
-plt.plot(new_waves,new_flux,'k')
+
+'''plt.plot(new_waves,new_flux,'k')
 plt.plot(blue_waves,blue_flux,'--')
 plt.plot(red_waves,red_flux,'r--')
-plt.show()
-'''
+plt.show()'''
 
 # Need to create some routine to match the blue/red continuum.
 # Try smoothing both sides, fitting blue with linear regression (weighted by ivar), then compare red side fit to
@@ -125,32 +127,54 @@ plt.show()
 # print(blue_waves[-300])
 num = 50
 
-blue_smooth_flux, blue_smooth_ivar = ivarsmooth(blue_flux[-num:-3], blue_ivars[-num:-3], 5)
-blue_smooth_flux = blue_smooth_flux.reshape((len(blue_flux[-num:-3]), 1))
-blue_smooth_waves = blue_waves[-num:-3].reshape((len(blue_waves[-num:-3]), 1))
+blue_hold_waves = blue_waves[blue_waves > 10][-num:]
+blue_smooth_flux, blue_smooth_ivar = ivarsmooth(blue_flux[blue_waves > 10][-num:],
+                                                blue_ivars[blue_waves > 10][-num:], 5)
+blue_smooth_flux = blue_smooth_flux.reshape((num, 1))
+blue_smooth_waves = blue_hold_waves.reshape((num, 1))
 
 blue_regr = LinearRegression()
 blue_regr.fit(blue_smooth_waves, blue_smooth_flux, blue_smooth_ivar)
 
-'''
-plt.plot(blue_waves,blue_flux,'--')
-plt.plot(blue_smooth_waves[:,0],blue_smooth_flux,'g')
-plt.plot(blue_smooth_waves[:,0],regr.predict(blue_smooth_waves),'r')
+plt.plot(blue_waves, blue_flux, '--')
+plt.plot(blue_smooth_waves[:, 0], blue_smooth_flux, 'g')
+plt.plot(blue_smooth_waves[:, 0], blue_regr.predict(blue_smooth_waves), 'r')
 plt.show()
-'''
 
-red_hold_waves = red_waves[red_waves>10][:num]
-red_smooth_flux, red_smooth_ivar = ivarsmooth(red_flux[red_waves>10][:num], red_ivars[red_waves>10][:num], 5)
+red_hold_waves = red_waves[red_waves > 10][:num]
+red_smooth_flux, red_smooth_ivar = ivarsmooth(red_flux[red_waves > 10][:num], red_ivars[red_waves > 10][:num], 5)
 red_smooth_flux = red_smooth_flux.reshape((num, 1))
 red_smooth_waves = red_hold_waves.reshape((num, 1))
 
 red_regr = LinearRegression()
 red_regr.fit(red_smooth_waves, red_smooth_flux, red_smooth_ivar)
 
-plt.plot(blue_waves,blue_flux,'b--',alpha=0.2)
-plt.plot(red_waves,red_flux,'r--',alpha=0.2)
-plt.plot(blue_smooth_waves[:,0],blue_regr.predict(blue_smooth_waves),'b')
-plt.plot(red_smooth_waves[:,0],blue_regr.predict(red_smooth_waves),'b')
-plt.plot(blue_smooth_waves[:,0],red_regr.predict(blue_smooth_waves),'r')
-plt.plot(red_smooth_waves[:,0],red_regr.predict(red_smooth_waves),'r')
+plt.plot(blue_waves, blue_flux, 'b--', alpha=0.2)
+plt.plot(red_waves, red_flux, 'r--', alpha=0.2)
+plt.plot(blue_smooth_waves[:, 0], blue_regr.predict(blue_smooth_waves), 'b')
+plt.plot(red_smooth_waves[:, 0], blue_regr.predict(red_smooth_waves), 'b')
+plt.plot(blue_smooth_waves[:, 0], red_regr.predict(blue_smooth_waves), 'r')
+plt.plot(red_smooth_waves[:, 0], red_regr.predict(red_smooth_waves), 'r')
 plt.show()
+
+# Just looked at overlap average for smoothed flux vs regression flux for star and ratios were identical to 0.0015.
+# Don't need regression?
+
+# For non-stellar spectrum, difference was large at 0.904 smooth vs 1.034 regr. Need regression?
+
+# Would think regression is better since variance is used as weight, but by eye smooth is better. Only 7 overlap values,
+# perhaps this is too small an overlap. Try another?
+
+# Also need to try varying number of samples for regression, width of smoothing, and weights for regression.
+
+lower_mask = blue_smooth_waves >= red_smooth_waves[0]
+upper_mask = red_smooth_waves <= blue_smooth_waves[-1]
+
+smooth_ratio = blue_smooth_flux[lower_mask].mean() / red_smooth_flux[upper_mask].mean()
+
+blue_regr_mean = blue_regr.predict(blue_smooth_waves[lower_mask].reshape(lower_mask.sum(), 1)).mean()
+red_regr_mean = red_regr.predict(red_smooth_waves[upper_mask].reshape(upper_mask.sum(), 1)).mean()
+
+regr_ratio = blue_regr_mean / red_regr_mean
+
+embed()
