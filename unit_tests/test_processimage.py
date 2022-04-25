48e5601701f9758ed39b7558bc2db0dac4d0bfbe
profxj <xaiver@ucolick.org>
2022-04-25 13:58:25 -0700

    procimg

--- unit_tests/test_processimage.py
+++ unit_tests/test_processimage.py
@@ -13,6 +13,9 @@ import numpy as np
 
 from pypeit.images import rawimage
 from pypeit.spectrographs.util import load_spectrograph
+from pypeit.core import procimg
+from pypeit.par.pypeitpar import ProcessImagesPar
+from pypeit import utils
 
 
 @pytest.fixture
@@ -84,3 +87,27 @@ def test_continuum_subtraction(kast_blue_arc_file):
     rawImage.subtract_continuum(force=True)
     # Test
     assert rawImage.steps['subtract_continuum']
+
+def test_lacosmic():
+    spec = load_spectrograph('keck_deimos')
+    file = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA', 'keck_deimos', '1200G_M_5500',
+                        'd0315_45929.fits')
+    par = ProcessImagesPar(use_biasimage=False, use_pixelflat=False, use_illumflat=False)
+    img = rawimage.RawImage(file, spec, 1)
+    pimg = img.process(par)
+    test_img = pimg.image[:500,:500]
+    test_var = utils.inverse(pimg.ivar[:500,:500])
+    crmask = procimg.lacosmic(test_img, varframe=test_var, maxiter=1)
+    assert np.sum(crmask) == 1240, 'L.A.Cosmic changed'
+
+    _crmask = procimg.lacosmic(test_img, varframe=test_var, maxiter=2)
+    assert np.sum(_crmask) > np.sum(crmask), '2nd iteration should find more cosmics'
+
+    _crmask = procimg.lacosmic(test_img, saturation=6000., varframe=test_var, maxiter=1)
+    assert np.sum(_crmask) < np.sum(crmask), 'Should have flagged some pixels as saturated'
+
+    __crmask = procimg.lacosmic(test_img, saturation=np.full(test_img.shape, 6000.),
+                                    varframe=test_var, maxiter=1)
+    assert np.array_equal(__crmask, _crmask), 'Saturation array failed.'
+
+
