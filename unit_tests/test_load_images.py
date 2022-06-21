73a21fd4c6069052ac66316c7b7f2f34257da0d1
Dusty Reichwein <dusty@ucolick.org>
2022-06-21 13:25:03 -0700

    Small fixes for windows

--- unit_tests/test_load_images.py
+++ unit_tests/test_load_images.py
@@ -66,8 +66,8 @@ def test_load_kast():
 
 
 def test_load_vlt_xshooter_uvb():
-    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/vlt_xshooter',
-                         'UVB_1x1/XSHOO.2010-04-28T05:34:32.723.fits.gz')
+    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','vlt_xshooter',
+                         'UVB_1x1','XSHOO.2010-04-28T05:34:32.723.fits.gz')
     try:
         data_img = grab_img('vlt_xshooter_uvb', ifile)
     except:
@@ -76,10 +76,10 @@ def test_load_vlt_xshooter_uvb():
 
 def test_load_vlt_xshooter_vis():
 
-    root = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/vlt_xshooter')
-    files = [ os.path.join(root, 'VIS_1x1/XSHOO.2010-04-28T05:34:37.853.fits.gz'),
-              os.path.join(root, 'VIS_2x1/XSHOO.2016-08-02T08:45:46.510.fits.gz'),
-              os.path.join(root, 'VIS_2x2/XSHOO.2016-10-08T00:51:04.703.fits.gz') ]
+    root = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','vlt_xshooter')
+    files = [ os.path.join(root, 'VIS_1x1','XSHOO.2010-04-28T05:34:37.853.fits.gz'),
+              os.path.join(root, 'VIS_2x1','XSHOO.2016-08-02T08:45:46.510.fits.gz'),
+              os.path.join(root, 'VIS_2x2','XSHOO.2016-10-08T00:51:04.703.fits.gz') ]
 
     for f in files:
         try:
@@ -88,15 +88,15 @@ def test_load_vlt_xshooter_vis():
             pytest.fail('VLT XSHOOTER VIS test data section failed: {0}'.format(f))
 
 def test_load_vlt_xshooter_nir():
-    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/vlt_xshooter',
-                         'NIR/XSHOO.2016-08-02T08:45:49.494.fits.gz')
+    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','vlt_xshooter',
+                         'NIR','XSHOO.2016-08-02T08:45:49.494.fits.gz')
     try:
         data_img = grab_img('vlt_xshooter_nir', ifile)
     except:
         pytest.fail('VLT XSHOOTER NIR test data section failed: {0}'.format(ifile))
 
 def test_load_gnirs():
-    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/gemini_gnirs/32_SB_SXD/',
+    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','gemini_gnirs','32_SB_SXD',
                          'cN20170331S0206.fits')
     try:
         data_img = grab_img('gemini_gnirs', ifile)
@@ -104,7 +104,7 @@ def test_load_gnirs():
         pytest.fail('Gemini GNIRS test data section failed: {0}'.format(ifile))
 
 def test_load_mage():
-    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/magellan_mage/1x1',
+    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','magellan_mage','1x1',
                          'mage0050.fits')
     try:
         data_img = grab_img('magellan_mage', ifile)
@@ -112,7 +112,7 @@ def test_load_mage():
         pytest.fail('Magellan MAGE test data section failed: {0}'.format(ifile))
 
 def test_load_gmos():
-    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/gemini_gmos/GS_HAM_R400_700',
+    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','gemini_gmos','GS_HAM_R400_700',
                          'S20181005S0086.fits.gz')
     try:
         data_img = grab_img('gemini_gmos_south_ham', ifile)
@@ -120,7 +120,7 @@ def test_load_gmos():
         pytest.fail('Gemini GMOS test data section failed: {0}'.format(ifile))
 
 def test_load_osiris():
-    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/gtc_osiris/R2500R',
+    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','gtc_osiris','R2500R',
                          '0002851159-20210217-OSIRIS-OsirisBias.fits')
     try:
         data_img = grab_img('gtc_osiris', ifile)
@@ -128,7 +128,7 @@ def test_load_osiris():
         pytest.fail('GTC OSIRIS test data section failed: {0}'.format(ifile))
 
 def test_load_bok():
-    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/bok_bc/600',
+    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','bok_bc','600',
                          'g0005.fits')
     try:
         data_img = grab_img('bok_bc', ifile)
@@ -136,7 +136,7 @@ def test_load_bok():
         pytest.fail('Bok BC test data section failed: {0}'.format(ifile))
 
 def test_load_efosc2():
-    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/ntt_efosc2/gr6',
+    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','ntt_efosc2','gr6',
                          'EFOSC.2020-02-12T02:03:38.359.fits')
     try:
         data_img = grab_img('ntt_efosc2', ifile)
@@ -144,7 +144,7 @@ def test_load_efosc2():
         pytest.fail('NTT/EFOSC2 test data section failed: {0}'.format(ifile))
 
 def test_load_goodman():
-    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/soar_goodman_red/M2',
+    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','soar_goodman_red','M2',
                          '0320_FRB210320_host_05-04-2021.fits.fz')
     try:
         data_img = grab_img('soar_goodman_red', ifile)
@@ -152,7 +152,7 @@ def test_load_goodman():
         pytest.fail('Bok BC test data section failed: {0}'.format(ifile))
 
 def test_load_deveny():
-    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/ldt_deveny/DV2',
+    ifile = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','ldt_deveny','DV2',
                          '20210522.0001.fits')
     try:
         data_img = grab_img('ldt_deveny', ifile)
@@ -171,7 +171,7 @@ def test_load_fire():
 '''
 @dev_suite_required
 def test_load_hires():
-    files = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/keck_hires/RED',
+    files = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA','keck_hires','RED',
                          'hires0009.fits.gz')
     proc = ProcessImages('keck_hires_red', par, files)
     proc.build_image()
