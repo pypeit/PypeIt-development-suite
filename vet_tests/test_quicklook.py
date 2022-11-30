39b801341e3c5523ba247863be07f22ae05f6fac
profxj <xaiver@ucolick.org>
2022-11-30 10:48:40 -0800

    vet deimous

--- vet_tests/test_quicklook.py
+++ vet_tests/test_quicklook.py
@@ -12,9 +12,7 @@ from pypeit import specobjs
 
 def test_shane_kast_ql(redux_out):
     instr = 'shane_kast_blue' 
-    outroot = os.path.join(redux_out,
-                             instr,
-                             '600_4310_d55')
+    outroot = os.path.join(redux_out, instr, '600_4310_d55')
 
     for test in ['std', 'masters', 'calibs', 'multi', 'nostack', 'boxcar']:
         outdir = os.path.join(outroot, f'QL_{test}')
@@ -47,4 +45,32 @@ def test_shane_kast_ql(redux_out):
         else:
             assert not np.isclose(sobjs.BOX_RADIUS[0], 4.651162790697675)
 
-    # Check boxcar -- 4.65116279
+def test_keck_deimos_ql(redux_out):
+
+    instr = 'keck_deimos' 
+    outroot = os.path.join(redux_out, instr, 
+                           '600ZD_M_6500')
+
+    for test in ['maskID', 'slitspatnum']:
+        outdir = os.path.join(outroot, f'QL_{test}')
+        rdxfolder = 'd1010_0056'
+        rdxdir = os.path.join(outdir, rdxfolder)
+        scidir = os.path.join(rdxdir, 'Science')
+
+        # PypeIt File
+        pypeit_file = os.path.join(rdxdir, f'{instr}_A.pypeit')
+        assert os.path.isfile(pypeit_file)
+        pypeitFile = PypeItFile.from_file(pypeit_file)
+        assert pypeitFile.config['rdx']['quicklook']
+
+        # Outputs
+        spec2d_files = glob.glob(os.path.join(scidir, 'spec2d*')) 
+        nfiles = 1 
+        assert len(spec2d_files) == nfiles
+        spec1d_files = glob.glob(os.path.join(scidir, 'spec1d*.fits')) 
+        assert len(spec1d_files) == nfiles
+
+        sobjs = specobjs.SpecObjs.from_fitsfile(spec1d_files[0])
+        assert sobjs.nobj == 1
+        assert sobjs.SLITID == 452
+        assert sobjs.MASKDEF_ID == 958454
