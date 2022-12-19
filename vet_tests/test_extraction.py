dfe7ab2bc77b782d970419613442e5f2aea87dc2
profxj <xavier@ucolick.org>
2022-12-19 14:41:09 -0800

    test

--- vet_tests/test_extraction.py
+++ vet_tests/test_extraction.py
@@ -10,96 +10,16 @@ from pypeit.pypmsgs import PypeItError
 from pypeit.inputfiles import PypeItFile
 from pypeit import specobjs
 
-def test_shane_kast_ql(redux_out):
-    instr = 'shane_kast_blue' 
-    outroot = os.path.join(redux_out, instr, '600_4310_d55')
+def test_bok_bc_manual(redux_out):
+    """ Checks that the manual extraction with FWHM is working for Bok BC"""
+    instr = 'bok_bc' 
+    rdxdir = os.path.join(redux_out, instr, '300')
+    scidir = os.path.join(rdxdir, 'Science')
 
-    for test in ['std', 'masters', 'calibs', 'multi', 'nostack', 'boxcar']:
-        outdir = os.path.join(outroot, f'QL_{test}')
-        if test in ['std', 'masters', 'calibs', 'boxcar']:
-            rdxfolder = 'b27'
-        else:
-            rdxfolder = 'b27-b28'
-        rdxdir = os.path.join(outdir, rdxfolder)
-        scidir = os.path.join(rdxdir, 'Science')
+    spec1d_files = glob.glob(os.path.join(scidir, 'spec1d*.fits')) 
+    spec1d_files.sort()
+    sobjs = specobjs.SpecObjs.from_fitsfile(spec1d_files[1]) # 0 should be the standard
 
-        # PypeIt File
-        pypeit_file = os.path.join(rdxdir, f'{instr}_A.pypeit')
-        assert os.path.isfile(pypeit_file)
-        pypeitFile = PypeItFile.from_file(pypeit_file)
-        assert pypeitFile.config['rdx']['quicklook']
-
-        # Outputs
-        spec2d_files = glob.glob(os.path.join(scidir, 'spec2d*')) 
-        nfiles = 2 if test in ['nostack'] else 1
-        assert len(spec2d_files) == nfiles
-        spec1d_files = glob.glob(os.path.join(scidir, 'spec1d*.fits')) 
-        assert len(spec1d_files) == nfiles
-
-        sobjs = specobjs.SpecObjs.from_fitsfile(spec1d_files[0])
-        assert sobjs.nobj == 1
-
-        # Additional
-        if test == 'boxcar':
-            assert np.isclose(sobjs.BOX_RADIUS[0], 4.651162790697675)
-        else:
-            assert not np.isclose(sobjs.BOX_RADIUS[0], 4.651162790697675)
-
-def test_keck_deimos_ql(redux_out):
-
-    instr = 'keck_deimos' 
-    outroot = os.path.join(redux_out, instr, 
-                           '600ZD_M_6500')
-
-    for test in ['maskID', 'slitspatnum']:
-        outdir = os.path.join(outroot, f'QL_{test}')
-        rdxfolder = 'd1010_0056'
-        rdxdir = os.path.join(outdir, rdxfolder)
-        scidir = os.path.join(rdxdir, 'Science')
-
-        # PypeIt File
-        pypeit_file = os.path.join(rdxdir, f'{instr}_A.pypeit')
-        assert os.path.isfile(pypeit_file)
-        pypeitFile = PypeItFile.from_file(pypeit_file)
-        assert pypeitFile.config['rdx']['quicklook']
-
-        # Outputs
-        spec2d_files = glob.glob(os.path.join(scidir, 'spec2d*')) 
-        nfiles = 1 
-        assert len(spec2d_files) == nfiles
-        spec1d_files = glob.glob(os.path.join(scidir, 'spec1d*.fits')) 
-        assert len(spec1d_files) == nfiles
-
-        sobjs = specobjs.SpecObjs.from_fitsfile(spec1d_files[0])
-        assert sobjs.nobj == 1
-        assert sobjs.SLITID == 452
-        assert sobjs.MASKDEF_ID == 958454
-
-def test_keck_lris_red_ql(redux_out):
-
-    instr = 'keck_lris_red' 
-    outroot = os.path.join(redux_out, instr, 
-                           'long_600_7500_d560')
-
-    for test in ['det']:
-        outdir = os.path.join(outroot, f'QL_{test}')
-        rdxfolder = 'LR.20160216.40478'
-        rdxdir = os.path.join(outdir, rdxfolder)
-        scidir = os.path.join(rdxdir, 'Science')
-
-        # PypeIt File
-        pypeit_file = os.path.join(rdxdir, f'{instr}_A.pypeit')
-        assert os.path.isfile(pypeit_file)
-        pypeitFile = PypeItFile.from_file(pypeit_file)
-        assert pypeitFile.config['rdx']['quicklook']
-
-        # Outputs
-        spec2d_files = glob.glob(os.path.join(scidir, 'spec2d*')) 
-        nfiles = 1 
-        assert len(spec2d_files) == nfiles
-        spec1d_files = glob.glob(os.path.join(scidir, 'spec1d*.fits')) 
-        assert len(spec1d_files) == nfiles
-
-        sobjs = specobjs.SpecObjs.from_fitsfile(spec1d_files[0])
-        assert sobjs.nobj == 2
-        assert sobjs.DET[0] == 'DET02'
+    hand_sobj = sobjs[sobjs.hand_extract_flag]
+    # Test
+    assert np.isclose(hand_sobj.BOX_RADIUS[0], 4.)  # Value in the pypeit file
