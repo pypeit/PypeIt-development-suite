dcaf990a7b24383c8b27f22583502dc98485d046
Kyle Westfall <westfall@ucolick.org>
2023-04-11 12:35:50 -0700

    ql work

--- unit_tests/test_metadata.py
+++ unit_tests/test_metadata.py
@@ -17,6 +17,77 @@ from pypeit import inputfiles
 from pypeit.calibrations import Calibrations
 
 
+def get_kast_blue_files():
+    # Check for files
+    file_root = Path(os.getenv('PYPEIT_DEV')) / 'RAW_DATA' / 'shane_kast_blue' / '600_4310_d55'
+    assert file_root.exists(), 'Bad raw data directory'
+    files = sorted(file_root.glob('b*'))
+    assert len(files) > 0
+    return files
+
+
+def test_rm_rows():
+    # Check for files
+    files = get_kast_blue_files()
+    # Init
+    ps = PypeItSetup(files, spectrograph_name='shane_kast_blue')
+    # Run
+    ps.run()
+    assert len(ps.fitstbl) == 26, 'Missing files'
+
+    # Find the biases
+    is_bias = ps.fitstbl.find_frames('bias', index=True)
+    assert len(is_bias) == 10, 'Should find 10 bias frames'
+
+    # Remove them
+    ps.fitstbl.remove_rows(is_bias)
+    assert len(ps.fitstbl) == 16, 'Should have removed 10 frames'
+    assert not any(ps.fitstbl.find_frames('bias')), 'Should not find any bias frames'
+
+    # TODO: Leaving this inconsistency for now.  BEWARE!!
+    assert len(ps.file_list) == 26, \
+            'Direct manipulation of fitstbl means PypeItSetup is not self-consistent'
+
+    # Rebuild
+    ps = PypeItSetup(files, spectrograph_name='shane_kast_blue')
+    ps.run()
+
+    # Remove the standard
+    is_std = ps.fitstbl.find_frames('standard', index=True)
+    assert is_std.size == 1, 'Should find one standard frame'
+    is_sci = ps.fitstbl.find_frames('science', index=True)
+    assert is_sci.size == 2, 'Should find two science frames'
+    assert np.array_equal(ps.fitstbl['comb_id'][is_std].data, [1]), 'Combination groups changed'
+    assert np.array_equal(ps.fitstbl['comb_id'][is_sci].data, [2,3]), 'Combination groups changed'
+
+    # Remove the standard, but don't regroup
+    ps.fitstbl.remove_rows(is_std)
+    assert len(ps.fitstbl) == 25, 'Should have removed one standard'
+    is_sci = ps.fitstbl.find_frames('science', index=True)
+    assert np.array_equal(ps.fitstbl['comb_id'][is_sci].data, [2,3]), 'Combination groups changed'
+
+    # Rebuild
+    ps = PypeItSetup(files, spectrograph_name='shane_kast_blue')
+    ps.run()
+    is_std = ps.fitstbl.find_frames('standard', index=True)
+    # Remove the standard and regroup
+    ps.fitstbl.remove_rows(is_std, regroup=True)
+    is_sci = ps.fitstbl.find_frames('science', index=True)
+    assert np.array_equal(ps.fitstbl['comb_id'][is_sci].data, [1,2]), \
+            'Combination group numbers not updated'
+
+    # Rebuild
+    ps = PypeItSetup(files, spectrograph_name='shane_kast_blue')
+    ps.run()
+
+    # Use the PypeItSetup wrapper
+    is_std = ps.fitstbl.find_frames('standard', index=True)
+    ps.remove_table_rows(is_std, regroup=True)
+    assert len(ps.fitstbl) == 25, 'Should have removed one standard'
+    assert len(ps.fitstbl) == len(ps.file_list), 'Length of file list should match metadata table'
+    # TODO: Need tests that test propagation of removed rows to frametype and usrdata
+
+
 def test_lris_red_multi_400():
     file_list = glob.glob(os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA', 'keck_lris_red',
                           'multi_400_8500_d560', '*.fits.gz'))
