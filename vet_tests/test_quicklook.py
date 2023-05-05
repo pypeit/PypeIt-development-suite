07d821d6ba1d54a202756a32ac2a7bf7c4bc701d
Kyle Westfall <westfall@ucolick.org>
2023-05-05 10:42:57 -0700

    remove nostack test

--- vet_tests/test_quicklook.py
+++ vet_tests/test_quicklook.py
@@ -15,7 +15,7 @@ def test_shane_kast_ql(redux_out):
     instr = 'shane_kast_blue' 
     outroot = os.path.join(redux_out, instr, '600_4310_d55')
 
-    for test in ['std', 'cooked', 'match', 'multi', 'nostack', 'boxcar']:
+    for test in ['std', 'cooked', 'match', 'multi', 'boxcar']:
         outdir = os.path.join(outroot, f'QL_{test}')
         if test in ['std', 'cooked', 'match', 'boxcar']:
             rdxfolder = 'b27'
@@ -32,7 +32,7 @@ def test_shane_kast_ql(redux_out):
 
         # Outputs
         spec2d_files = glob.glob(os.path.join(scidir, 'spec2d*')) 
-        nfiles = 2 if test in ['nostack'] else 1
+        nfiles = 1
         assert len(spec2d_files) == nfiles
         spec1d_files = glob.glob(os.path.join(scidir, 'spec1d*.fits')) 
         assert len(spec1d_files) == nfiles
