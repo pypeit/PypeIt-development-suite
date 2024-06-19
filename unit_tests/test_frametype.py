7f0fe02719615b65f4b0993b5da4c245d6a9dfcd
Debora Pelliccia <dpelliccia@ucolick.org>
2024-06-19 01:48:27 -0700

    added unit tests for hires

--- unit_tests/test_frametype.py
+++ unit_tests/test_frametype.py
@@ -464,6 +464,64 @@ def test_lris_blue_orig():
         shutil.rmtree(output_path)
 
 
+def test_hires():
+    # Raw HIRES directory
+    dev_root = Path(os.getenv('PYPEIT_DEV')).resolve()
+    assert dev_root.exists(), f'PypeIt development suite directory does not exist: {dev_root}'
+
+    raw_dir = dev_root / 'RAW_DATA' / 'keck_hires'
+    assert raw_dir.exists(), f'Raw data directory does not exist: {raw_dir}'
+
+    # Get the list of setup directories
+    setups_names = ['lJ0306+1853_U074_RED_C2_ECH_0.72_XD_1.42_1x3',
+                    'J1218+2951_U116Hr_RED_C5_ECH_-0.22_XD_0.21_1x2']
+    setups = np.concatenate([sorted(raw_dir.glob(s)) for s in setups_names]).tolist()
+
+    # Set the output path and *remove if it already exists
+    output_path = Path('.').resolve() / 'output'
+    if output_path.exists():
+        shutil.rmtree(output_path)
+
+    # Iterate through the setups
+    for setup in setups:
+        # Find the relevant pypeit file constructed by hand.
+        by_hand_pypeit = dev_root / 'pypeit_files' / f'keck_hires_{setup.name.lower()}.pypeit'
+
+        if not by_hand_pypeit.exists():
+            # It doesn't exist, so assume there is no by-hand pypeit
+            # file to compare to
+            continue
+
+        # Run pypeit_setup
+        ps = PypeItSetup.from_file_root(str(setup), 'keck_hires')
+        ps.run(setup_only=True)
+
+        # Write the automatically generated pypeit data
+        pypeit_files = ps.fitstbl.write_pypeit(output_path, cfg_lines=ps.user_cfg)
+
+        # Read the frame types from both the by-hand and automated
+        # pypeit files
+        byhand_pypeItFile = PypeItFile.from_file(by_hand_pypeit)
+        auto_pypeItFile = PypeItFile.from_file(pypeit_files[0])
+
+        # For each file in the by-hand list, check that the frame types
+        # in the automatically generated pypeit file are identical
+        for f in byhand_pypeItFile.frametypes.keys():
+            type_list = np.sort(byhand_pypeItFile.frametypes[f].split(','))
+            if 'science' in type_list or 'standard' in type_list:
+                # Only ensuring that calibrations are correctly typed
+                continue
+            assert f in auto_pypeItFile.frametypes.keys(), \
+                'Frame {0} not automatically parsed for setup {1}.'.format(f, setup)
+            assert np.array_equal(type_list, np.sort(auto_pypeItFile.frametypes[f].split(','))), \
+                'Frame types differ for file {0} in setup {1}\n'.format(f, setup) \
+                + '    By-hand types: {0}'.format(byhand_pypeItFile.frametypes[f]) \
+                + '    Automated types: {0}'.format(auto_pypeItFile.frametypes[f])
+
+        # Clean up after every setup
+        shutil.rmtree(output_path)
+
+
 def test_vet_assigned_ftypes():
 
     dev_root = Path(os.getenv('PYPEIT_DEV')).resolve()
