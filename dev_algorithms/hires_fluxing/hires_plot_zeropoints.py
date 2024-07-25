cdf3cf4731ee18fb74b61499148ecdc45f6d224c
Debora Pelliccia <dpelliccia@ucolick.org>
2024-07-24 14:09:45 -1000

    added hires_zeropoint_collection.py script

--- dev_algorithms/hires_fluxing/hires_plot_zeropoints.py
+++ dev_algorithms/hires_fluxing/hires_plot_zeropoints.py
@@ -1,17 +1,23 @@
+# after running the script hires_zeropoint_collection.py, run this script to plot the
+# collection of zeropoints in a single figure.
+
+
+
 import os
 from pathlib import Path
 from pypeit import sensfunc
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.colors as mcolors
 from IPython import embed
 
 # for plots
-mcolors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
 plt.rc('font', family='serif')
 plt.rcParams['mathtext.default'] = u'regular'
 plt.rc('axes', labelsize=15)
-plt.rc('legend', fontsize=9, borderpad=1., handlelength=1., handleheight=1., labelspacing=0.5, handletextpad=0.5,
+plt.rc('legend', fontsize=7, borderpad=1., handlelength=1., handleheight=1., labelspacing=0.5, handletextpad=0.5,
        borderaxespad=0.5, columnspacing=0.)
 plt.rc('xtick', labelsize=12, direction='in', top=True)
 plt.rc('xtick.major', size=8, width=1.)
@@ -27,7 +33,7 @@ plt.rc('axes', linewidth=1.5)
 hires_redux = Path('/Users/dpelliccia/Desktop/adap2020/')
 # grab all the sens*.fits files
 # sens_files = list(hires_redux.glob('*/sens*.fits'))
-sens_files = list(hires_redux.glob('*/*/*/*/*/sens*.fits'))
+sens_files = list(hires_redux.glob('*/*/sens*.fits'))
 
 # list of sens objects
 sensobjs_list = []
@@ -40,9 +46,16 @@ for sfile in sens_files:
     sensobjs_list.append(_sens)
     wmins.append(_sens.sens['WAVE_MIN'].data.min())
     wmaxs.append(_sens.sens['WAVE_MAX'].data.max())
-    # datasets.append('_'.join(sfile.parent.name.split('_')[1:]))
-    datasets.append(sfile.parent.parent.parent.parent.parent.name)
-    
+    datasets.append(sfile.parent.name)
+
+# colors for the plots
+colors_values = np.array(list(mcolors.CSS4_COLORS.values()))
+# exclude light colors. Colors that have v (of hsv) values less than 0.85 are kept
+hsv_v = np.array([mcolors.rgb_to_hsv(mcolors.to_rgba(c)[:3])[-1] for c in colors_values])
+pcolors = colors_values[hsv_v < 0.9]
+
+
+# wave ranges for the plots
 plot_wmin = np.min(wmins) #- 50.
 plot_wmax = np.max(wmaxs) #+ 50.
 wv_break = (plot_wmax - plot_wmin)/6.
@@ -73,22 +86,22 @@ with PdfPages(plotname) as pdf:
                     wv_gpm = wave > 1.0
                     # rejmask = zeropoint_data_gpm[wv_gpm] & np.logical_not(zeropoint_fit_gpm[wv_gpm])
                     axis.plot(wave[wv_gpm], zeropoint_data[wv_gpm], drawstyle='steps-mid',
-                              color=mcolors[i], linewidth=0.6, alpha=0.4, zorder=5)
+                              color=pcolors[i], linewidth=0.6, alpha=0.4, zorder=5)
                     if nplot == 0:
                         axis.plot(wave[wv_gpm], zeropoint_fit[wv_gpm], label=f'{stdname} - {datasets[i]}',
-                                  color=mcolors[i], linewidth=1.3, alpha=1., zorder=0)
+                                  color=pcolors[i], linewidth=1.3, alpha=1., zorder=0)
                     else:
-                        axis.plot(wave[wv_gpm], zeropoint_fit[wv_gpm], color=mcolors[i], linewidth=2.0, alpha=1., zorder=0)
+                        axis.plot(wave[wv_gpm], zeropoint_fit[wv_gpm], color=pcolors[i], linewidth=2.0, alpha=1., zorder=0)
                     # axis.plot(wave[wv_gpm][rejmask], zeropoint_data[wv_gpm][rejmask], 's', zorder=10, mfc='None',
                     #           mec='blue', mew=0.7, label='rejected pixels from fit')
                     # axis.plot(wave[wv_gpm][np.logical_not(zeropoint_data_gpm[wv_gpm])],
                     #           zeropoint_data[wv_gpm][np.logical_not(zeropoint_data_gpm[wv_gpm])], 'v',
                     #           zorder=11, mfc='None', mec='orange', mew=0.7, label='originally masked')
                     nplot += 1
-        axis.set_ylim(10,21)
-        axis.legend()
+        axis.set_ylim(9,21)
+        axis.legend(loc='lower left')
         axis.set_xlabel('Wavelength')
         axis.set_ylabel('Zeropoint (AB mag)')
         fig.tight_layout()
-        pdf.savefig()
+        pdf.savefig(dpi=60)
         # plt.show()
