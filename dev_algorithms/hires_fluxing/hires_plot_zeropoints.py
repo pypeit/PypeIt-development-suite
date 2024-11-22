7b8d9670488a1f5cf66cb9a94f03d7e97feb706b
Debora Pelliccia <dpelliccia@ucolick.org>
2024-11-21 17:41:28 -1000

    updated script

--- dev_algorithms/hires_fluxing/hires_plot_zeropoints.py
+++ dev_algorithms/hires_fluxing/hires_plot_zeropoints.py
@@ -27,10 +27,13 @@ plt.rc('ytick.major', size=8, width=0.8)
 plt.rc('ytick.minor', size=4, width=0.8)
 plt.rc('axes', linewidth=1.5)
 
+plot_by_order = True
+
 
 # REDUX/keck_hires path
 # hires_redux = Path(os.getenv('PYPEIT_DEV') + '/REDUX_OUT/keck_hires/')
-hires_redux = Path('/Users/dpelliccia/Desktop/adap2020/')
+# hires_redux = Path('/Users/dpelliccia/Desktop/adap2020/')
+hires_redux = Path('/Volumes/GoogleDrive/Shared drives/PypeIt ADAP 2020/sensfuncs/')
 # grab all the sens*.fits files
 # sens_files = list(hires_redux.glob('*/sens*.fits'))
 sens_files = list(hires_redux.glob('*/*/sens*.fits'))
@@ -40,6 +43,7 @@ sensobjs_list = []
 wmins = []
 wmaxs = []
 datasets = []
+all_orders = np.array([])
 
 for sfile in sens_files:
     _sens = sensfunc.SensFunc.from_file(sfile, chk_version=False)
@@ -47,6 +51,9 @@ for sfile in sens_files:
     wmins.append(_sens.sens['WAVE_MIN'].data.min())
     wmaxs.append(_sens.sens['WAVE_MAX'].data.max())
     datasets.append(sfile.parent.name)
+    all_orders = np.append(all_orders, _sens.sens['ECH_ORDERS'].data)
+
+order_vec = np.arange(all_orders.min(), all_orders.max()+1, dtype=int) if plot_by_order else [1]
 
 # colors for the plots
 colors_values = np.array(list(mcolors.CSS4_COLORS.values()))
@@ -56,52 +63,64 @@ pcolors = colors_values[hsv_v < 0.9]
 
 
 # wave ranges for the plots
-plot_wmin = np.min(wmins) #- 50.
-plot_wmax = np.max(wmaxs) #+ 50.
-wv_break = (plot_wmax - plot_wmin)/6.
-wave_ranges = [(plot_wmin, plot_wmin + wv_break), (plot_wmin + wv_break, plot_wmin + 2*wv_break),
-               (plot_wmin + 2*wv_break, plot_wmin + 3*wv_break), (plot_wmin + 3*wv_break, plot_wmin + 4*wv_break),
-               (plot_wmin + 4*wv_break, plot_wmin + 5*wv_break), (plot_wmin + 5*wv_break, plot_wmax)]
+plot_wmin = np.min(wmins)
+plot_wmax = np.max(wmaxs)
+if plot_by_order:
+    wave_ranges = [(plot_wmin, plot_wmax)]
+else:
+    wv_break = (plot_wmax - plot_wmin)/6.
+    wave_ranges = [(plot_wmin, plot_wmax - 5*wv_break), (plot_wmin + wv_break, plot_wmax - 4*wv_break),
+                   (plot_wmin + 2*wv_break, plot_wmax - 3*wv_break), (plot_wmin + 3*wv_break, plot_wmax - 2*wv_break),
+                   (plot_wmin + 4*wv_break, plot_wmax - wv_break), (plot_wmin + 5*wv_break, plot_wmax)]
 
 # plot the zeropoints
-plotname = 'collection_zeropoints.pdf'
+plotname = 'collection_zeropoints.pdf' if plot_by_order is False else 'collection_zeropoints_by_order.pdf'
+
 with PdfPages(plotname) as pdf:
     for wr in wave_ranges:
         _wmin, _wmax = wr
-        fig = plt.figure(figsize=(23, 6.))
-        axis = plt.subplot()
-        plt.minorticks_on()
-        for i,sensobj in enumerate(sensobjs_list):
-            nplot = 0
-            stdname = sensobj.std_name
-            sens_tab = sensobj.sens
-            for sens in sens_tab:
-                if sens['WAVE_MIN'] > _wmin and sens['WAVE_MAX'] <= _wmax:
-                    wave = sens['SENS_WAVE']
-                    zeropoint_data = sens['SENS_ZEROPOINT']
-                    zeropoint_data_gpm = sens['SENS_ZEROPOINT_GPM']
-                    zeropoint_fit = sens['SENS_ZEROPOINT_FIT']
-                    zeropoint_fit_gpm = sens['SENS_ZEROPOINT_FIT_GPM']
+        for iord in order_vec[::-1]:
+            fig = plt.figure(figsize=(23, 6.))
+            axis = plt.subplot()
+            plt.minorticks_on()
+            for i,sensobj in enumerate(sensobjs_list):
+                nplot = 0
+                stdname = sensobj.std_name
+                sens_tab = sensobj.sens
+                for sens in sens_tab:
+                    if (sens['ECH_ORDERS'] == iord) if plot_by_order else (sens['WAVE_MIN'] >= _wmin and sens['WAVE_MAX'] <= _wmax):
+                        wave = sens['SENS_WAVE']
+                        zeropoint_data = sens['SENS_ZEROPOINT']
+                        zeropoint_data_gpm = sens['SENS_ZEROPOINT_GPM']
+                        zeropoint_fit = sens['SENS_ZEROPOINT_FIT']
+                        zeropoint_fit_gpm = sens['SENS_ZEROPOINT_FIT_GPM']
 
-                    wv_gpm = wave > 1.0
-                    # rejmask = zeropoint_data_gpm[wv_gpm] & np.logical_not(zeropoint_fit_gpm[wv_gpm])
-                    axis.plot(wave[wv_gpm], zeropoint_data[wv_gpm], drawstyle='steps-mid',
-                              color=pcolors[i], linewidth=0.6, alpha=0.4, zorder=5)
-                    if nplot == 0:
-                        axis.plot(wave[wv_gpm], zeropoint_fit[wv_gpm], label=f'{stdname} - {datasets[i]}',
-                                  color=pcolors[i], linewidth=1.3, alpha=1., zorder=0)
-                    else:
-                        axis.plot(wave[wv_gpm], zeropoint_fit[wv_gpm], color=pcolors[i], linewidth=2.0, alpha=1., zorder=0)
-                    # axis.plot(wave[wv_gpm][rejmask], zeropoint_data[wv_gpm][rejmask], 's', zorder=10, mfc='None',
-                    #           mec='blue', mew=0.7, label='rejected pixels from fit')
-                    # axis.plot(wave[wv_gpm][np.logical_not(zeropoint_data_gpm[wv_gpm])],
-                    #           zeropoint_data[wv_gpm][np.logical_not(zeropoint_data_gpm[wv_gpm])], 'v',
-                    #           zorder=11, mfc='None', mec='orange', mew=0.7, label='originally masked')
-                    nplot += 1
-        axis.set_ylim(9,21)
-        axis.legend(loc='lower left')
-        axis.set_xlabel('Wavelength')
-        axis.set_ylabel('Zeropoint (AB mag)')
-        fig.tight_layout()
-        pdf.savefig(dpi=60)
-        # plt.show()
+                        wv_gpm = wave > 1.0
+                        # rejmask = zeropoint_data_gpm[wv_gpm] & np.logical_not(zeropoint_fit_gpm[wv_gpm])
+                        axis.plot(wave[wv_gpm], zeropoint_data[wv_gpm], drawstyle='steps-mid',
+                                  color=pcolors[i], linewidth=0.6, alpha=0.4, zorder=5)
+                        if nplot == 0:
+                            label = f'{stdname} - {datasets[i]}' if plot_by_order is False \
+                                else f'{stdname} - {datasets[i]} - order {iord}'
+                            axis.plot(wave[wv_gpm], zeropoint_fit[wv_gpm], label=label,
+                                      color=pcolors[i], linewidth=1.3, alpha=1., zorder=0)
+                        else:
+                            axis.plot(wave[wv_gpm], zeropoint_fit[wv_gpm], color=pcolors[i], linewidth=2.0, alpha=1., zorder=0)
+                        # axis.plot(wave[wv_gpm][rejmask], zeropoint_data[wv_gpm][rejmask], 's', zorder=10, mfc='None',
+                        #           mec='blue', mew=0.7, label='rejected pixels from fit')
+                        # axis.plot(wave[wv_gpm][np.logical_not(zeropoint_data_gpm[wv_gpm])],
+                        #           zeropoint_data[wv_gpm][np.logical_not(zeropoint_data_gpm[wv_gpm])], 'v',
+                        #           zorder=11, mfc='None', mec='orange', mew=0.7, label='originally masked')
+                        nplot += 1
+            if len(list(axis.get_lines())) == 0:
+                plt.close()
+                continue
+            axis.set_ylim(9,21)
+            # axis.set_xlim(_wmin, _wmax)
+            axis.legend()
+            axis.set_xlabel('Wavelength')
+            axis.set_ylabel('Zeropoint (AB mag)')
+            fig.tight_layout()
+            pdf.savefig(dpi=60)
+            # plt.show()
+
