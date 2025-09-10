996c600262af095e72a45678764dfb515b82cb70
Timothy Ellsworth-Bowers <tbowers@lowell.edu>
2025-09-10 11:22:14 -0700

    Add corrected ``.pypeit`` file and Unit Test
    The ``.pypeit`` file was not updated to match the new spectrograph
    class.
    
    The new DevSuite Unit Test tests the part of ``pypeit_check_for_calibs``
    that is corrected in https://github.com/pypeit/PypeIt/pull/1971 .  Those
    corrections fixed bugs that would have caused errors if they had been
    tested for.
    
    	modified:   pypeit_files/gemini_gnirs_ifu_lr_ifu_32mm.pypeit
    	modified:   unit_tests/test_chk_calibs.py

--- unit_tests/test_chk_calibs.py
+++ unit_tests/test_chk_calibs.py
@@ -2,18 +2,18 @@
 Module to run tests on scripts
 """
 import os
-import shutil
-
-from IPython import embed
-import pytest
-
-import numpy as np
+import pathlib
 
 import matplotlib
-matplotlib.use('agg')  # For Travis
+import numpy as np
 
 from pypeit.scripts.chk_for_calibs import ChkForCalibs
 from pypeit.tests.tstutils import data_output_path
+
+from IPython import embed
+
+matplotlib.use('agg')  # For Travis
+
 
 def test_chk_calibs_not():
     os.chdir(data_output_path(''))
@@ -40,4 +40,25 @@ def test_chk_calibs_deimos():
     answers, _ = ChkForCalibs.main(pargs)
     assert np.all(answers['pass'][:-1]), 'One or more failures!'
 
+def test_chk_calibs_save_setups():
+    os.chdir(data_output_path(''))
+    # 830G
+    droot = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA/keck_deimos/830G_M_8600/')
+    pargs = ChkForCalibs.parse_args([droot, '-s', 'keck_deimos', '--save_setups'])
+    answers, ps = ChkForCalibs.main(pargs)
+    assert np.all(answers['pass'][:-1]), 'One or more failures!'
+
+    # Check for the created setup files
+    output_path = pathlib.Path().absolute() / 'setup_files'
+    assert output_path.is_dir()
+    pypeit_file = output_path / ps.pypeit_file
+    assert pypeit_file.is_file()
+    assert pypeit_file.with_suffix('.sorted').is_file()
+    assert pypeit_file.with_suffix('.calib').is_file()
+
+    # Delete all created files
+    pypeit_file.unlink()
+    pypeit_file.with_suffix('.sorted').unlink()
+    pypeit_file.with_suffix('.calib').unlink()
+    output_path.unlink()
 
