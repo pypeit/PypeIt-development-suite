282376156f4c76b48c14f37113958171699f7bbc
Debora Pelliccia <dpelliccia@ucolick.org>
2026-05-27 19:49:34 -1000

    added coadd2d

--- test_scripts/test_setups.py
+++ test_scripts/test_setups.py
@@ -266,6 +266,8 @@ _coadd2d = {
         'VIS_manual': [dict(coadd_file=True)]},
     'jwst_nirspec': {
         'MSA_PRISM_nomosaic': [dict(coadd_file=True)]},
+    'jwst_nirspec': {
+        'MSA_G395M_F290LP_mosaic': [dict(coadd_file=True)]},
     'keck_deimos': {
         '830G_M_9000_dither': [dict(coadd_file=True)]},
     'keck_mosfire': {
